3bc36301e79b7f08afe90c623fb7188a7eea18ce
jim winstead <jimw@trainedmonkey.com>
2011-01-11 11:57:56 -0800

    Add basic item receipt and load from vendor order

--- txn.php
+++ txn.php
@@ -8,9 +8,56 @@ $number= (int)$_REQUEST['number'];
 
 if (!$type || !$number) die("no transaction specified.");
 
+switch ($type) {
+case 'vendor';
+?>
+<script>
+$.expr[":"].match = function(obj, index, meta, stack){
+  return (obj.textContent || obj.innerText || $(obj).text() || "") == meta[3];
+}
+$(function() {
+  $('#receive #search').focus()
+  $('#receive .submit').click(function() {
+    $('#receive .error').hide(); // hide old error messages
+    var q = $("#receive #search").val();
+    $.ajax({
+      url: "txn-item-receive.php",
+      dataType: "json",
+      data: ({ type: "<?=$type?>", number: "<?=$number?>", search: q }),
+      success: function(data) {
+        if (data.error) {
+          $("#receive .error").html("<p>" + data.error + "</p>");
+          $("#receive .error").show();
+        } else {
+          // update table
+          var row= $(".sortable td.num:match(" + data.line + ")").parent()
+          $(row).children(":eq(6)").text(data.ordered)
+          $(row).children(":eq(7)").text(data.shipped)
+          $(row).children(":eq(8)").text(data.allocated)
+          $("#receive #search").val("");
+        }
+      }
+    });
+    return false;
+  });
+});
+</script>
+<form id="receive" action="txn-item-receive.php" method="post">
+ <div class="error" style="display: none"></div>
+ <input id="type" type="hidden" name="type" value="<?=ashtml($type)?>">
+ <input id="number" type="hidden" name="number" value="<?=ashtml($number)?>">
+ <input id="search" type="text" name="search">
+ <input class="submit" type="submit" name="Receive">
+</form>
+<?
+  break;
+}
+
+
 $type= $db->real_escape_string($type);
 
 $q= "SELECT
+            line AS `#\$num`,
             item.code Code\$item,
             item.name Name,
             retail_price MSRP\$dollar,
@@ -26,6 +73,7 @@ $q= "SELECT
               WHEN 'relative' THEN CONCAT('$', discount, ' off')
             END Discount,
             ordered as Ordered,
+            shipped as Shipped,
             allocated as Allocated
        FROM txn
        LEFT JOIN txn_line ON (txn.id = txn_line.txn)
@@ -35,3 +83,18 @@ $q= "SELECT
 
 dump_table($db->query($q));
 dump_query($q);
+
+switch ($type) {
+case 'vendor';
+?>
+<form enctype="multipart/form-data" action="txn-load-mac.php" method="post">
+ <input type="hidden" name="type" value="<?=ashtml($type)?>">
+ <input type="hidden" name="number" value="<?=ashtml($number)?>">
+ <input type="file" name="src">
+ <br>
+ <input type="submit" name="Import from MacPhersons">
+</form>
+<?
+  break;
+}
+
