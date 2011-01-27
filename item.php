98be1954dcc4d707e27c4670ecacc4545676bd0c
jim winstead <jimw@trainedmonkey.com>
2011-01-26 17:34:37 -0800

    Add item listing with history

---
+++ item.php
@@ -0,0 +1,77 @@
+<?
+require 'scat.php';
+
+head("item");
+
+$code= $_GET['code'];
+$id= (int)$_GET['id'];
+?>
+<form method="get" action="items.php">
+<input id="focus" type="text" name="code" value="<?=htmlspecialchars($code)?>">
+<input type="submit" value="Find Items">
+</form>
+<br>
+<?
+
+if (!$code && !$id) exit;
+
+if (!$id && $code) {
+  $r= $db->query("SELECT id FROM item WHERE code = '" .
+                 $db->real_escape_string($code) . "'");
+  if (!$r) die($m->error);
+
+  if (!$r->num_rows)
+      die("<h2>No item found.</h2>");
+
+  $id= $r->fetch_row();
+  $id= $id[0];
+}
+
+$q= "SELECT
+            item.code Code\$item,
+            item.name Name,
+            brand.name Brand,
+            retail_price MSRP\$dollar,
+            IF(discount_type,
+               CASE discount_type
+                 WHEN 'percentage' THEN ROUND(retail_price * ((100 - discount) / 100), 2)
+                 WHEN 'relative' THEN (retail_price - discount) 
+                 WHEN 'fixed' THEN (discount)
+               END,
+               NULL) Sale\$dollar,
+            CASE discount_type
+              WHEN 'percentage' THEN CONCAT(ROUND(discount), '% off')
+              WHEN 'relative' THEN CONCAT('$', discount, ' off')
+            END Discount,
+            (SELECT SUM(allocated) FROM txn_line WHERE item = item.id) Stock\$right,
+            minimum_quantity Minimum\$right
+       FROM item
+       JOIN brand ON (item.brand = brand.id)
+  LEFT JOIN barcode ON (item.id = barcode.item)
+      WHERE item.id = $id
+   GROUP BY item.id";
+
+dump_table($db->query($q));
+#dump_query($q);
+
+$r= $db->query("SET @count = 0");
+
+$q= "SELECT DATE_FORMAT(created, '%a, %b %e %Y %H:%i') Date,
+            txn AS Transaction,
+            CASE type
+              WHEN 'customer' THEN IF(allocated < 0, 'Sale', 'Return')
+              WHEN 'vendor' THEN 'Stock'
+              WHEN 'internal' THEN 'Correction'
+              ELSE type
+            END Type,
+            allocated AS Quantity\$right,
+            @count := @count + allocated AS Count\$right
+       FROM txn_line
+       JOIN txn ON (txn_line.txn = txn.id)
+      WHERE item = $id
+      GROUP BY txn
+      ORDER BY created";
+
+echo '<h2>History</h2>';
+dump_table($db->query($q));
+dump_query($q);
