378d13c5e7bcd29b6cceee5eaebee324744f1fca
jim winstead <jimw@trainedmonkey.com>
2011-03-14 21:50:36 -0700

    Add basic search for transactions (only searches on name/company)

--- txns.php
+++ txns.php
@@ -3,21 +3,44 @@ require 'scat.php';
 
 head("transactions");
 
+$criteria= array();
+
 $type= $_REQUEST['type'];
 if ($type) {
-  $criteria= "type = '".$db->real_escape_string($type)."'";
-} else {
+  $criteria[]= "(type = '".$db->real_escape_string($type)."')";
+}
+
+$q= $_REQUEST['q'];
+if ($q) {
+  $criteria[]= "(person.name LIKE '%$q%'
+             OR person.company LIKE '%$q%')";
+}
+
+if (empty($criteria)) {
   $criteria= '1=1';
+} else {
+  $criteria= join(' AND ', $criteria);
 }
 
 ?>
+<form method="get" action="txns.php">
+<select name="type">
+ <option value="">Any
+ <option value="customer">Invoice
+ <option value="vendor">Purchase Order
+ <option value="internal">Internal
+</select>
+<input id="focus" type="text" name="q" value="<?=ashtml($q)?>">
+<input type="submit" value="Search">
+</form>
+<br>
 <form method="get" action="txn.php">
 <select name="type">
  <option value="customer">Invoice
  <option value="vendor">Purchase Order
  <option value="internal">Internal
 </select>
-<input id="focus" type="text" name="number" value="">
+<input type="text" name="number" value="">
 <input type="submit" value="Look Up">
 </form>
 <br>
