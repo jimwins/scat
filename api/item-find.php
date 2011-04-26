46d74abe7836e8d1a2ca7519786d49cee7ecc51a
jim winstead <jimw@trainedmonkey.com>
2011-04-25 21:00:05 -0700

    Start roughing in basic order-building functionality

---
+++ api/item-find.php
@@ -0,0 +1,61 @@
+<?
+include '../scat.php';
+
+$q= $_GET['q'];
+if (!$q) exit;
+
+$terms= preg_split('/\s+/', $q);
+$criteria= array();
+foreach ($terms as $term) {
+  $term= $db->real_escape_string($term);
+  if (preg_match('/^code:(.+)/i', $term, $dbt)) {
+    $criteria[]= "(item.code LIKE '{$dbt[1]}%')";
+  } else {
+    $criteria[]= "(item.name LIKE '%$term%'
+               OR brand.name LIKE '%$term%'
+               OR item.code LIKE '%$term%'
+               OR barcode.code LIKE '%$term%')";
+  }
+}
+# allow option to include inactive and/or deleted
+if (!$_REQUEST['all']) {
+  $criteria[]= "(active AND NOT deleted)";
+} else {
+  $criteria[]= "(NOT deleted)";
+}
+
+$q= "SELECT
+            item.code code,
+            item.name name,
+            brand.name brand,
+            retail_price msrp,
+            IF(discount_type,
+               CASE discount_type
+                 WHEN 'percentage' THEN ROUND(retail_price * ((100 - discount) / 100), 2)
+                 WHEN 'relative' THEN (retail_price - discount) 
+                 WHEN 'fixed' THEN (discount)
+               END,
+               NULL) price,
+            CASE discount_type
+              WHEN 'percentage' THEN CONCAT(ROUND(discount), '% off')
+              WHEN 'relative' THEN CONCAT('$', discount, ' off')
+            END discount,
+            (SELECT SUM(allocated) FROM txn_line WHERE item = item.id) stock
+       FROM item
+  LEFT JOIN brand ON (item.brand = brand.id)
+  LEFT JOIN barcode ON (item.id = barcode.item)
+      WHERE " . join(' AND ', $criteria) . "
+   GROUP BY item.id";
+
+$r= $db->query($q);
+if (!$r) {
+  die(json_encode(array('error' => 'Query failed. ' . $db->error,
+                        'query' => $q)));
+}
+
+$items= array();
+while ($row= $r->fetch_assoc()) {
+  $items[]= $row;
+}
+
+echo json_encode($items);
--- index.php
+++ index.php
@@ -3,7 +3,96 @@ require 'scat.php';
 
 head("Scat");
 ?>
-<form method="get" action="items.php">
-<input id="focus" type="text" name="q" size="100" value="<?=htmlspecialchars($q)?>">
+<script>
+var snd= new Object;
+snd.yes= new Audio("./sound/yes.wav");
+snd.no= new Audio("./sound/no.wav");
+snd.maybe= new Audio("./sound/maybe.wav");
+
+$.expr[":"].match = function(obj, index, meta, stack){
+    return (obj.textContent || obj.innerText || $(obj).text() || "") == meta[3];
+}
+
+var lastItem;
+
+$(function() {
+  $('#lookup').submit(function() {
+    $('#items .error').hide(); // hide old error messages
+    $('#lookup input[name="q"]').focus().select();
+
+    var q = $('#lookup input[name="q"]').val();
+
+    // short integer and recently scanned? adjust quantity
+    if (q.length < 3 && lastItem && parseInt(q) > 0) {
+      $(lastItem).children(":eq(0)").text(parseInt(q));
+      return false;
+    }
+
+    // add to existing line item?
+
+    // go find!
+
+    $.ajax({
+      url: "api/item-find.php",
+      dataType: "json",
+      data: ({ q: q }),
+      success: function(data) {
+        if (data.error) {
+          snd.no.play();
+          $("#items .error").html("<p>" + data.error + "</p>");
+          $("#items .error").show();
+        } else {
+          if (data.length == 0) {
+            snd.no.play();
+          } else if (data.length == 1) {
+            snd.yes.play();
+
+            // look for matching row
+            var row= $("#items td:match(" + data[0].code + ")").parent()
+
+            // have one? just increment quantity
+            if (row.length) {
+              var qty= parseInt($(row).children(":eq(0)").text());
+              $(row).children(":eq(0)").text(++qty);
+              var ext= qty * parseFloat($(row).children(":eq(3)").text());
+              $(row).children(":eq(4)").text(ext.toFixed(2));
+              lastItem= row;
+            }
+            // otherwise add the row
+            else {
+              // build name/description
+              var desc = data[0].name;
+              if (data[0].discount) {
+                desc+= '<br><small>MSRP $' + data[0].msrp + ' / ' + data[0].discount + '</small>';
+              }
+
+              // add the new row
+              $('#items tbody').append('<tr valign="top"><td align="center">1</td><td align="center">' + data[0].code + '</td><td>' + desc + '</td><td class="dollar right">' + data[0].price + '</td><td class="dollar right">' + data[0].price + '</td></tr>');
+              lastItem= $('#items tbody tr:first');
+            }
+
+          } else {
+            snd.maybe.play();
+          }
+        }
+      }
+    });
+
+    return false;
+  });
+});
+</script>
+<form id="lookup" method="get" action="items.php">
+<input autofocus type="text" name="q" size="100" autocomplete="off" placeholder="Scan item or enter search terms" value="<?=htmlspecialchars($q)?>">
 <input type="submit" value="Find Items">
 </form>
+<div id="items">
+ <div class="error"></div>
+ <table width="80%">
+ <thead>
+  <tr><th>Qty</th><th>Code</th><th width="50%">Name</th><th>Price</th><th>Ext</th></tr>
+ </thead>
+ <tbody>
+ </tbody>
+</table>
+</div>
