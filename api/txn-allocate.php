6aee0964d833b6637db21b4419cc906ac16fba97
jim winstead <jimw@trainedmonkey.com>
2011-12-30 00:47:52 -0800

    Don't allow a paid order to be allocated, set the filled time

--- api/txn-allocate.php
+++ api/txn-allocate.php
@@ -5,7 +5,22 @@ $txn= (int)$_REQUEST['txn'];
 if (!$txn)
   die_jsonp("no transaction specified.");
 
+$q= "SELECT paid FROM txn WHERE id = $txn";
+$r= $db->query($q)
+  or die_jsonp($db->error);
+
+$row= $r->fetch_row();
+
+if ($row[0]) {
+  die_jsonp("This order is already paid!");
+}
+
 $q= "UPDATE txn_line SET allocated = ordered WHERE txn = $txn";
+$r= $db->query($q)
+  or die_jsonp($db->error);
+$lines= $r->num_rows;
+
+$q= "UPDATE txn SET filled = NOW() WHERE id = $txn";
 $r= $db->query($q)
   or die_jsonp($db->error);
 
