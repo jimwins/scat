e947416541b180777d6c8b4e49c4985fe54011ae
jim winstead <jimw@trainedmonkey.com>
2012-01-10 18:43:10 -0800

    Show amount due when we have a partial payment (duh)

--- index.php
+++ index.php
@@ -344,7 +344,7 @@ function updateTotal() {
 
   var paid_date= $('#txn').data('paid_date');;
   var paid= $('#txn').data('paid');;
-  if (paid_date != null) {
+  if (paid || paid_date != null) {
     $('#items #due').text(amount(Math.abs(total - paid)));
     $('#due-row').show();
   } else {
