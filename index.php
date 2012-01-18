73de46d1997840c168481441395ea8f2317942e2
jim winstead <jimw@trainedmonkey.com>
2012-01-17 19:17:24 -0800

    Set 'over' class on <td>, not the <span> or it screws up in-place edit

--- index.php
+++ index.php
@@ -159,9 +159,9 @@ function updateItems(items) {
 function updateRow(row) {
   $('.quantity', row).text(row.data('quantity'));
   if (row.data('quantity') > row.data('stock')) {
-    $('.quantity', row).addClass('over');
+    $('.quantity', row).parent().addClass('over');
   } else {
-    $('.quantity', row).removeClass('over');
+    $('.quantity', row).parent().removeClass('over');
   }
   $('.code', row).text(row.data('code'));
   $('.name', row).text(row.data('name'));
