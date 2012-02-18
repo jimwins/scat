d5b485a9c11e7696317b379874715c175e17ea97
jim winstead <jimw@trainedmonkey.com>
2012-02-18 15:35:42 -0800

    Handle null amounts correctly

--- js/scat.js
+++ js/scat.js
@@ -26,7 +26,7 @@ function round_to_even(num, decimalPlaces) {
 
 // format number as $3.00 or ($3.00)
 function amount(amount) {
-  if (typeof(amount) == 'undefined') {
+  if (typeof(amount) == 'undefined' || amount == null) {
     return '';
   }
   if (amount < 0.0) {
