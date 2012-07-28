27da378b45969743a19df504291d6af646643f57
jim winstead <jimw@trainedmonkey.com>
2012-07-27 18:37:55 -0700

    Fix queries to change bulk pricing of MXG- products

--- api/txn-apply-discounts.php
+++ api/txn-apply-discounts.php
@@ -20,20 +20,20 @@ $count= $db->get_one("SELECT ABS(SUM(ordered))
 if ($count >= 50) {
   $q= "UPDATE txn_line, item
           SET txn_line.discount = 40.0, txn_line.discount_type = 'percentage'
-        WHERE txn = $id AND code LIKE 'MXG-%'";
+        WHERE txn = $id AND txn_line.item = item.id AND code LIKE 'MXG-%'";
   $db->query($q)
     or die_query($db, $q);
 } elseif ($count >= 12) {
   $q= "UPDATE txn_line, item
           SET txn_line.discount = 30.0, txn_line.discount_type = 'percentage'
-        WHERE txn = $id AND code LIKE 'MXG-%'";
+        WHERE txn = $id AND txn_line.item = item.id AND code LIKE 'MXG-%'";
   $db->query($q)
     or die_query($db, $q);
 } else {
   $q= "UPDATE txn_line, item
           SET txn_line.discount = item.discount,
               txn_line.discount_type = item.discount_type
-        WHERE txn = $id AND code LIKE 'MXG-%'";
+        WHERE txn = $id AND txn_line.item = item.id AND code LIKE 'MXG-%'";
   $db->query($q)
     or die_query($db, $q);
 }
