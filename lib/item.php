ec01c956915627bb3161d2d34eac0fa392e101d2
jim winstead <jimw@trainedmonkey.com>
2012-08-25 19:36:27 -0700

    Wrap item_load() around item_find() to remove duplicate code

--- lib/item.php
+++ lib/item.php
@@ -128,61 +128,6 @@ function item_find($db, $q, $options) {
 }
 
 function item_load($db, $id) {
-  $q= "SELECT
-              item.id, item.code, item.name,
-              brand.id brand_id, brand.name brand,
-              retail_price retail_price,
-              IF(item.discount_type,
-                 CASE item.discount_type
-                   WHEN 'percentage' THEN ROUND(retail_price * ((100 - item.discount) / 100), 2)
-                   WHEN 'relative' THEN (retail_price - item.discount) 
-                   WHEN 'fixed' THEN (item.discount)
-                 END,
-                 NULL) sale_price,
-              item.discount_type, item.discount,
-              CASE item.discount_type
-                WHEN 'percentage' THEN CONCAT(ROUND(item.discount), '% off')
-                WHEN 'relative' THEN CONCAT('$', item.discount, ' off')
-              END discount_label,
-              (SELECT SUM(allocated) FROM txn_line WHERE item = item.id) stock,
-              (SELECT retail_price
-                 FROM txn_line JOIN txn ON (txn_line.txn = txn.id)
-                WHERE txn_line.item = item.id AND txn.type = 'vendor'
-                  AND filled IS NOT NULL
-                ORDER BY filled DESC
-                LIMIT 1) last_net,
-              minimum_quantity,
-              GROUP_CONCAT(CONCAT(barcode.code, '!', barcode.quantity)
-                           SEPARATOR ',') barcodes,
-              active
-         FROM item
-    LEFT JOIN brand ON (item.brand = brand.id)
-    LEFT JOIN barcode ON (item.id = barcode.item)
-        WHERE item.id = $id
-     GROUP BY item.id";
-
-  $r= $db->query($q)
-    or die_query($db, $q);
-
-  $item= $r->fetch_assoc();
-
-  if ($item['retail_price'])
-    $item['retail_price']= (float)$item['retail_price'];
-  if ($item['sale_price'])
-    $item['sale_price']= (float)$item['sale_price'];
-  if ($item['last_net'])
-    $item['last_net']= (float)$item['last_net'];
-  if ($item['minimum_quantity'])
-    $item['minimum_quantity']= (int)$item['minimum_quantity'];
-  if ($item['stock'])
-    $item['stock']= (int)$item['stock'];
-
-  $barcodes= explode(',', $item['barcodes']);
-  $item['barcode']= array();
-  foreach ($barcodes as $barcode) {
-    list($code, $quantity)= explode('!', $barcode);
-    $item['barcode'][$code]= $quantity;
-  }
-
-  return $item;
+  $items= item_find($db, "item:$id", 0);
+  return $items[0];
 }
