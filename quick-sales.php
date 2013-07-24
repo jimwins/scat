fadb47b64abf263aac74ae9e99fd6f9f73b5115e
jim winstead <jimw@trainedmonkey.com>
2013-07-24 02:20:50 -0700

    Set begin/end date via variables

--- quick-sales.php
+++ quick-sales.php
@@ -16,6 +16,9 @@ $q= $_REQUEST['q'];
 $options= FIND_OR;
 list($sql_criteria, $begin) = item_terms_to_sql($db, $q, $options);
 
+$begin= '2012-07-01';
+$end=   '2012-11-01';
+
 $q= "select 
             item.id AS meta,
             item.code Code\$item,
@@ -24,7 +27,7 @@ $q= "select
             sum(-1 * allocated) Sold,
             avg(sale_price(txn_line.retail_price, txn_line.discount_type, txn_line.discount)) AvgPrice\$dollar,
             SUM(-1 * allocated * sale_price(txn_line.retail_price, txn_line.discount_type, txn_line.discount)) Total\$dollar
-       FROM txn left join txn_line on txn.id = txn_line.txn left join item on txn_line.item = item.id where type = 'customer' and ($sql_criteria) and paid between '2012-06-01' and '2013-01-01' group by 1";
+       FROM txn left join txn_line on txn.id = txn_line.txn left join item on txn_line.item = item.id where type = 'customer' and ($sql_criteria) and paid between '$begin' and '$end' group by 1";
 
 dump_table($db->query($q));
 
