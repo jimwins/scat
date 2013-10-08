9cf104ac3e5ab4a212f1105566a86dd3d1745454
jim winstead <jimw@trainedmonkey.com>
2013-10-08 15:07:18 -0700

    Fix generation of UPC barcodes

--- lib/php-barcode.php
+++ lib/php-barcode.php
@@ -390,14 +390,14 @@ class BarcodeUPC {
         if (strlen($code) < 12) {
             $code = '0' . $code;
         }
-        return BarcodeEAN::getDigit($code, 'ean13');
+        return BarcodeEAN::getDigit("0$code", 'ean13');
     }
 
     static public function compute($code){
         if (strlen($code) < 12) {
             $code = '0' . $code;
         }
-        return substr(BarcodeEAN::compute($code, 'ean13'), 1);
+        return substr(BarcodeEAN::compute("0$code", 'ean13'), 1);
     }
 }
 
