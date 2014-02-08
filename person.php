3d2cc27f484a8d1f3c89cd402aba07e4cd1c2b2d
jim winstead <jimw@trainedmonkey.com>
2014-02-07 20:16:35 -0800

    Turn off autocomplete

--- person.php
+++ person.php
@@ -13,7 +13,7 @@ $search= $_REQUEST['search'];
     <span class="input-group-btn">
       <input type="submit" class="btn btn-primary" value="Search">
     </span>
-    <input id="focus" type="text" class="form-control" size="60" name="search" data-bind="value: search" placeholder="Name">
+    <input id="focus" autocomplete="off" type="text" class="form-control" size="60" name="search" data-bind="value: search" placeholder="Name">
     <span class="input-group-addon">
       <label><input type="checkbox" value="1" name="all" data-bind="checked: all"> Include inactive?</label>
     </span>
