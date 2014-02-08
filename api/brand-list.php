5ede8e484124628222c3d5c3ac331e53ed507ae4
jim winstead <jimw@trainedmonkey.com>
2014-02-07 20:15:32 -0800

    Add 'verbose' mode for brand list to preserve item order

--- api/brand-list.php
+++ api/brand-list.php
@@ -1,14 +1,22 @@
 <?php
 require '../scat.php';
 
+$verbose= (int)$_REQUEST['verbose'];
+
 $q= "SELECT id, name FROM brand WHERE name != '' ORDER BY name";
 $r= $db->query($q)
   or die_query($db, $q);
 
 $brands= array();
 while ($row= $r->fetch_row()) {
-  $brands[$row[0]]= $row[1];
+  if ($verbose) {
+    $brands[]= array('id' => $row[0], 'name' => $row[1]);
+  } else {
+    $brands[$row[0]]= $row[1];
+  }
 }
-$brands['selected']= $_REQUEST['id'];
+
+if ($_REQUEST['id'])
+  $brands['selected']= $_REQUEST['id'];
 
 echo jsonp($brands);
