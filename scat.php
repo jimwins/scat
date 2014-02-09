dc17dbe37b425b6bcc7c6b60eef03510667fa9a3
jim winstead <jimw@trainedmonkey.com>
2014-02-09 14:05:20 -0800

    Add viewport meta tag to be mobile-friendly (maybe)

--- scat.php
+++ scat.php
@@ -27,6 +27,7 @@ header("content-type: text/html;charset=utf-8");?>
 <html>
 <head>
  <title><?=ashtml($title)?></title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
  <link rel="stylesheet" type="text/css" href="css/datepicker3.css">
