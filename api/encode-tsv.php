6af363e7d1731c702b9ca9a2a11662e30cef6f44
jim winstead <jimw@trainedmonkey.com>
2014-02-15 21:09:59 -0800

    Allow filename of download to be specified

--- api/encode-tsv.php
+++ api/encode-tsv.php
@@ -1,5 +1,9 @@
 <?
+$fn= "order.txt";
+if (preg_match('/^([-A-Za-z0-9_])+\\.([A-Za-z0-9]+)$/', $_REQUEST['fn'])) {
+  $fn= $_REQUEST['fn'];
+}
 header('Content-Type: application/tsv'); 
-header('Content-Disposition: attachment; filename="order.txt"'); 
+header('Content-Disposition: attachment; filename="' . $fn . '"'); 
 $file = urldecode($_REQUEST['file']); 
 echo $file; 
