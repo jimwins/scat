3d843abde80c57c117559d2eed68354637497ed5
jim winstead <jimw@trainedmonkey.com>
2014-02-17 11:06:08 -0800

    Preserve form value

--- item-searchform.php
+++ item-searchform.php
@@ -3,7 +3,7 @@
     <span class="input-group-btn">
       <input type="submit" class="btn btn-primary" value="Search">
     </span>
-    <input id="autofocus" type="text" class="form-control" size="60" name="search" data-bind="value: search" placeholder="Enter keywords or scan barcode">
+    <input id="autofocus" type="text" class="form-control" size="60" name="search" data-bind="value: search" placeholder="Enter keywords or scan barcode" value="<?=ashtml($_REQUEST['search'])?>">
     <span class="input-group-addon">
       <label><input type="checkbox" value="1" name="all" data-bind="checked: all"> Include inactive?</label>
     </span>
