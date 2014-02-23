0d60c25e5e5f841145e6e940a4e299ff4af51896
jim winstead <jimw@trainedmonkey.com>
2014-02-23 04:05:16 -0800

    Preserve value of checkbox

--- item-searchform.php
+++ item-searchform.php
@@ -3,9 +3,16 @@
     <span class="input-group-btn">
       <input type="submit" class="btn btn-primary" value="Search">
     </span>
-    <input id="autofocus" type="text" class="form-control" size="60" name="search" data-bind="value: search" placeholder="Enter keywords or scan barcode" value="<?=ashtml($_REQUEST['search'])?>">
+    <input id="autofocus" type="text" class="form-control" size="60"
+           name="search" data-bind="value: search"
+           placeholder="Enter keywords or scan barcode"
+           value="<?=ashtml($_REQUEST['search'])?>">
     <span class="input-group-addon">
-      <label><input type="checkbox" value="1" name="all" data-bind="checked: all"> Include inactive?</label>
+      <label>
+        <input type="checkbox" value="1" name="all" data-bind="checked: all"
+               <?=(int)$_REQUEST['all'] ? 'checked' : ''?>>
+          Include inactive?
+        </label>
     </span>
   </div>
 </form>
