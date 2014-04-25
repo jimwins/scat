32d7c20f0786c1613d3343f01cb026156ef1e05b
jim winstead <jimw@trainedmonkey.com>
2014-04-24 18:46:51 -0700

    Set brand to 'New Item', set barcode if given, link to vendor items

--- api/item-add.php
+++ api/item-add.php
@@ -17,14 +17,31 @@ $code= $db->escape($code);
 $name= $db->escape($name);
 $msrp= $db->escape($msrp);
 
+$brand= $db->get_one("SELECT id FROM brand WHERE name = 'New Item'");
+
 $q= "INSERT INTO item
         SET code = '$code', name = '$name', retail_price = '$msrp',
+            brand = $brand,
             taxfree = 0, minimum_quantity = 1, active = 1";
 
 $r= $db->query($q)
   or die_query($db, $q);
 
 $item_id= $db->insert_id;
+
+if ($_REQUEST['barcode']) {
+  $bar= $db->escape($_REQUEST['barcode']);
+  $q= "INSERT INTO barcode SET code = '$bar', item = $item_id, quantity = 1";
+  $r= $db->query($q)
+    or die_query($db, $q);
+}
+
+/* Link to vendor items */
+$q= "UPDATE vendor_item
+        SET vendor_item.item = $item_id
+      WHERE vendor_item.code = '$code'";
+$db->query($q)
+  or die_query($db, $q);
 
 $item= item_load($db, $item_id);
 
