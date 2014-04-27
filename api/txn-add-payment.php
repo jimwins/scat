7967a3520d8e88845333af9b3549527b86351dbf
jim winstead <jimw@trainedmonkey.com>
2014-04-27 01:13:59 -0700

    Use addPayment and removePayment on Transaction

--- api/txn-add-payment.php
+++ api/txn-add-payment.php
@@ -2,8 +2,6 @@
 include '../scat.php';
 include '../lib/txn.php';
 
-bcscale(2);
-
 $id= (int)$_REQUEST['id'];
 
 if (!$id)
@@ -12,76 +10,39 @@ if (!$id)
 $method= $_REQUEST['method'];
 $amount= $_REQUEST['amount'];
 
-$extra_fields= "";
-
 // validate method
 if (!in_array($method,
               array('cash','credit','square','stripe','dwolla','gift','check','discount','bad','donation','internal'))) {
   die_jsonp("Invalid method specified.");
 }
 
-$txn= txn_load($db, $id);
+$txn= new Transaction($db, $id);
+
+$extra= array(); // extra payment info
 
 // handle % discounts
 if ($method == 'discount') {
   if (preg_match('!^(/)?\s*(\d+)(%|/)?\s*$!', $amount, $m)) {
     if ($m[1] || $m[3]) {
-      $amount= round($txn['total'] * $m[2] / 100,
+      $amount= round($txn->total * $m[2] / 100,
                      2, PHP_ROUND_HALF_EVEN);
-      $extra_fields= "discount = $m[2],";
+      $extra['discount']= $m[2];
     }
   }
-}
-
-// if set, allow overpayment and create a 'change' record
-$change= (bool)($_REQUEST['change'] != 'false');
-
-// if no change and amount + paid > total, barf
-if (!$change &&
-    (($txn['total'] >= 0 && bccomp(bcadd($amount, $txn['total_paid']), $txn['total']) > 0) || ($txn['total'] < 0 && bccomp(bcadd($amount, $txn['total_paid']), $txn['total']) < 0))) {
-  die_jsonp("Amount is too much.");
 }
 
 if ($method == 'credit') {
   $cc= array();
   foreach(array('cc_txn', 'cc_approval', 'cc_lastfour',
                 'cc_expire', 'cc_type') as $field) {
-    $cc[]= "$field = '" . $db->real_escape_string($_REQUEST[$field]) . "', ";
+    $extra[$field]= $_REQUEST[$field];
   }
-
-  $extra_fields= join('', $cc);
 }
 
-// add payment record
-$q= "INSERT INTO payment
-        SET txn = $id, method = '$method', amount = $amount,
-        $extra_fields
-        processed = NOW()";
-$r= $db->query($q)
-  or die_query($db, $q);
-
-$payment= $db->insert_id;
-
-// if total > 0 and amount + paid > total, add change record
-$change_paid= 0.0;
-if ($change && $txn['total'] > 0 &&
-    bccomp(bcadd($amount, $txn['total_paid']), $txn['total']) > 0) {
-  $change_paid= bcsub($txn['total'],bcadd($amount,$txn['total_paid']));
-
-  $q= "INSERT INTO payment
-          SET txn = $id, method = 'change', amount = $change_paid,
-          processed = NOW()";
-  $r= $db->query($q)
-    or die_query($db, $q);
-}
-
-$txn['total_paid'] = bcadd($txn['total_paid'], bcadd($amount, $change_paid));
-
-// if we're all paid up, record that the txn is paid
-if (!bccomp($txn['total_paid'], $txn['total'])) {
-  $q= "UPDATE txn SET paid = NOW() WHERE id = $id";
-  $r= $db->query($q)
-    or die_query($db, $q);
+try {
+  $payment= $txn->addPayment($method, $amount, $extra);
+} catch (Exception $e) {
+  die_jsonp($e->getMessage());
 }
 
 echo jsonp(array('payment' => $payment,
--- api/txn-remove-payment.php
+++ api/txn-remove-payment.php
@@ -2,8 +2,6 @@
 include '../scat.php';
 include '../lib/txn.php';
 
-bcscale(2);
-
 $txn_id= (int)$_REQUEST['txn'];
 $id= (int)$_REQUEST['id'];
 $admin= (int)$_REQUEST['admin'];
@@ -11,27 +9,13 @@ $admin= (int)$_REQUEST['admin'];
 if (!$txn_id || !$id)
   die_jsonp("No transaction or payment specified.");
 
-$txn= txn_load($db, $txn_id);
+$txn= new Transaction($db, $txn_id);
 
-if ($txn['paid'] && !$admin)
-  die_jsonp("Transaction is fully paid, can't remove payments.");
-
-$db->start_transaction()
-  or die_query($db, "START TRANSACTION");
-
-// add payment record
-$q= "DELETE FROM payment WHERE id = $id AND txn = $txn_id";
-$r= $db->query($q)
-  or die_query($db, $q);
-
-if ($txn['paid']) {
-  $q= "UPDATE txn SET paid = NULL WHERE id = $txn_id";
-  $r= $db->query($q)
-    or die_query($db, $q);
+try {
+  $txn->removePayment($id, $admin);
+} catch (Exception $e) {
+  die_jsonp($e->getMessage());
 }
-
-$db->commit()
-  or die_query($db, "COMMIT");
 
 echo jsonp(array('txn' => txn_load($db, $txn_id),
                           'payments' => txn_load_payments($db, $txn_id)));
