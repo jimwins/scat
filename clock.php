a9b4f29dfe26b82bd7d6b10991eff2c7dccf2627
jim winstead <jimw@trainedmonkey.com>
2014-10-20 17:40:02 -0700

    Add clock page

---
+++ clock.php
@@ -0,0 +1,12 @@
+<?
+include 'scat.php';
+
+head("Clock @ Scat", true);
+?>
+<iframe src="http://www.payrollcenter.com/onpay/emp_clock_sm.html"
+        width="100%" height="800px" frameborder="0"
+        title="Clock">
+</iframe>
+<?
+foot();
+
