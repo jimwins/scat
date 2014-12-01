004eef4c5f16d9914fe7fa98b72584fea6648020
jim winstead <jimw@trainedmonkey.com>
2014-12-01 14:27:39 -0800

    Handle SLS assortments

--- api/txn-upload-mac.php
+++ api/txn-upload-mac.php
@@ -112,6 +112,23 @@ if (preg_match('/^linenum,qty/', $line)) {
     or die_query($db, $q);
 
   echo "Loaded ", $db->affected_rows, " rows from file.<br>";
+} elseif (preg_match('/^sls_sku/', $line)) {
+  // SLS
+  $q= "LOAD DATA LOCAL INFILE '$fn'
+       INTO TABLE vendor_order
+       FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'
+       LINES TERMINATED BY '\n'
+       IGNORE 1 LINES
+       (item_no, @cust_sku, description, @vendor_name,
+        msrp, net, @reg_discount, @promo_price, @promo_discount,
+        barcode, @upc2, @upc2_qty, @upc3, @upc3_qty, @min_ord_qty,
+        @level1, @level2, @level3, @level4, @level5, @ltl_only, @add_date,
+        @qty)
+       SET ordered = @qty, shipped = @qty";
+  $db->query($q)
+    or die_query($db, $q);
+
+  echo "Loaded ", $db->affected_rows, " rows from file.<br>";
 } elseif (preg_match('/^,Name,MSRP/', $line)) {
   // CSV
   $q= "LOAD DATA LOCAL INFILE '$fn'
