98bdee0529491975c94a1b9c5048f39307b17123
jim winstead <jimw@trainedmonkey.com>
2015-08-19 11:55:23 -0700

    Rough draft of reporting by brand

---
+++ report-brand.php
@@ -0,0 +1,119 @@
+<?
+require 'scat.php';
+require 'lib/item.php';
+
+$sql_criteria= "1=1";
+if (($items= $_REQUEST['items'])) {
+  list($sql_criteria, $x)= item_terms_to_sql($db, $_REQUEST['items'],
+                                             FIND_OR|FIND_ALL);
+}
+
+$begin= $_REQUEST['begin'];
+$end= $_REQUEST['end'];
+
+if (!$begin) {
+  $begin= date('Y-m-d', time());
+} else {
+  $begin= $db->escape($begin);
+}
+
+if (!$end) {
+  $end= date('Y-m-d', time());
+} else {
+  $end= $db->escape($end);
+}
+
+head("Brand Sales @ Scat", true);
+?>
+<form id="report-params" class="form-horizontal" role="form"
+      action="<?=$_SERVER['PHP_SELF']?>">
+  <div class="form-group">
+    <label for="datepicker" class="col-sm-2 control-label">
+      Dates
+    </label>
+    <div class="col-sm-10">
+      <div class="input-daterange input-group" id="datepicker">
+        <input type="text" class="form-control" name="begin"
+               value="<?=ashtml($begin)?>" />
+        <span class="input-group-addon">to</span>
+        <input type="text" class="form-control" name="end"
+               value="<?=ashtml($end)?>" />
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="items" class="col-sm-2 control-label">
+      Items
+    </label>
+    <div class="col-sm-10">
+      <input id="items" name="items" type="text"
+             class="form-control" style="width: 20em"
+             value="<?=ashtml($items)?>">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <input type="submit" class="btn btn-primary" value="Show">
+    </div>
+  </div>
+</form>
+<div id="results">
+<?
+
+$q= "SELECT
+            brand.id AS meta,
+            brand.name Name\$name,
+            SUM(-1 * allocated) Sold,
+            AVG(sale_price(txn_line.retail_price, txn_line.discount_type,
+                           txn_line.discount)) AvgPrice\$dollar,
+            SUM(-1 * allocated * sale_price(txn_line.retail_price,
+                                            txn_line.discount_type,
+                                            txn_line.discount)) Total\$dollar
+       FROM txn
+       LEFT JOIN txn_line ON txn.id = txn_line.txn
+       LEFT JOIN item ON txn_line.item = item.id
+       LEFT JOIN brand ON item.brand = brand.id
+      WHERE type = 'customer'
+        AND ($sql_criteria)
+        AND filled BETWEEN '$begin' AND '$end' + INTERVAL 1 DAY
+      GROUP BY 1
+      ORDER BY 2";
+
+dump_table($db->query($q));
+
+dump_query($q);
+?>
+  <button id="download" class="btn btn-default">Download</button>
+</div>
+<form id="post-csv" style="display: none"
+      method="post" action="api/encode-tsv.php">
+  <input type="hidden" name="fn" value="brand-sales.tsv">
+  <textarea id="file" name="file"></textarea>
+</form>
+<?
+foot();
+?>
+<script>
+$(function() {
+  $('#report-params .input-daterange').datepicker({
+      format: "yyyy-mm-dd",
+      todayHighlight: true
+  });
+});
+
+$('#download').on('click', function(ev) {
+  var tsv= "Brand\tQuantity\tAvgPrice\tTotal\r\n";
+  $.each($("#results tr"), function (i, row) {
+    if (i > 0) {
+      tsv += $('td:nth(1)', row).text() + "\t" +
+             $('td:nth(2)', row).text() + "\t" +
+             $('td:nth(3)', row).text() + "\t" +
+             $('td:nth(4)', row).text() +
+             "\r\n";
+    }
+  });
+  $("#file").val(tsv);
+  $("#post-csv").submit();
+});
+</script>
+
