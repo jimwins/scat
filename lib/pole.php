350a88cf19dd8acfdab3beff705a05177627311c
jim winstead <jimw@trainedmonkey.com>
2016-01-19 13:42:30 -0800

    Reset display before each message

--- lib/pole.php
+++ lib/pole.php
@@ -4,7 +4,7 @@ function pole_display_price($label, $price) {
   $sock= socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
   if (@socket_connect($sock, '127.0.0.1', 1888)) {
     socket_write($sock,
-                 sprintf("\x0d\x0a%-19.19s\x0a\x0d$%18.2f ",
+                 sprintf("\x1f\x0d\x0a%-19.19s\x0a\x0d$%18.2f ",
                          $label, $price));
   }
 }
