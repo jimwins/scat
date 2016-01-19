dd5105609caefab07ff37a0b9b30143fe8dbb558
jim winstead <jimw@trainedmonkey.com>
2016-01-18 16:58:41 -0800

    Add controls to limit list of items

--- reorder.php
+++ reorder.php
@@ -7,7 +7,7 @@ $extra= $extra_field= $extra_field_name= '';
 $code_field= "code";
 
 $vendor= (int)$_REQUEST['vendor'];
-if ($vendor) {
+if ($vendor > 0) {
   $code_field= "(SELECT code FROM vendor_item WHERE vendor = $vendor AND item = item.id LIMIT 1)";
   $extra= "AND EXISTS (SELECT id
                          FROM vendor_item
@@ -15,8 +15,8 @@ if ($vendor) {
                           AND item = item.id)";
   $extra_field= "(SELECT MIN(IF(promo_price, promo_price, net_price)) FROM vendor_item WHERE item = item.id AND vendor = $vendor) <= (SELECT MIN(IF(promo_price, promo_price, net_price)) FROM vendor_item WHERE item = item.id) Cheapest\$bool, ";
   $extra_field_name= "Cheapest\$bool,";
-}
-if ((int)$_REQUEST['novendor']) {
+} else if ($vendor < 0) {
+  // No vendor
   $extra= "AND NOT EXISTS (SELECT id
                              FROM vendor_item
                             WHERE item = item.id)";
@@ -30,11 +30,39 @@ if ($code) {
 <style>
 .order { text-align: right; }
 </style>
-<button id="download" class="btn btn-default">Download</button>
-<?if ($vendor) {?>
-  <button id="create" class="btn btn-default">Create Order</button>
-<?}?>
-<button id="zero" class="btn btn-default">Zero</button>
+<form class="form-inline" method="get" action="<?=$_SERVER['PHP_SELF']?>">
+  <div class="form-group">
+    <label class="sr-only" for="vendor"></label>
+    <select class="form-control" name="vendor">
+      <option value="">All vendors</option>
+      <option value="-1" <?if ($vendor == -1) echo 'selected'?>>
+        No vendor
+      </option>
+<?
+$q= "SELECT id, company FROM person WHERE role = 'vendor' ORDER BY company";
+$r= $db->query($q);
+
+while ($row= $r->fetch_assoc()) {
+  echo '<option value="', $row['id'], '"',
+       ($row['id'] == $vendor) ? ' selected' : '',
+       '>',
+       ashtml($row['company']),
+       '</option>';
+}
+?>
+    </select>
+  </div>
+  <div class="form-group">
+    <label class="sr-only" for="code">Code</label>
+    <input type="text" class="form-control" name="code" placeholder="Code"
+           value="<?=ashtml($code)?>">
+  </div>
+  <button type="submit" class="btn btn-primary">Limit</button>
+</form>
+
+<div class="pull-right">
+  <button id="zero" class="btn btn-default">Zero</button>
+</div>
 <?
 
 $q= "SELECT meta, Code\$item, Name, Stock\$right,
@@ -76,8 +104,11 @@ $q= "SELECT meta, Code\$item, Name, Stock\$right,
       ";
 
 dump_table($db->query($q));
-dump_query($q);
 ?>
+<button id="download" class="btn btn-default">Download</button>
+<?if ($vendor > 0) {?>
+  <button id="create" class="btn btn-default">Create Order</button>
+<?}?>
 <form id="post-csv" style="display: none"
       method="post" action="api/encode-tsv.php">
 <textarea id="file" name="file"></textarea>
