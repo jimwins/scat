bf878700ddfd3bdc3f55469f019db99be15e3ce9
jim winstead <jimw@trainedmonkey.com>
2016-05-25 19:35:46 -0700

    Make brand report more useful for comparisons

--- report-brand.php
+++ report-brand.php
@@ -59,37 +59,113 @@ head("Brand Sales @ Scat", true);
 </form>
 <div id="results">
 <?
-
-$q= "SELECT
-            brand.id AS meta,
-            brand.name Name\$name,
-            SUM(-1 * allocated) Sold,
-            AVG(sale_price(txn_line.retail_price, txn_line.discount_type,
-                           txn_line.discount)) AvgPrice\$dollar,
+/* Current */
+$q= "CREATE TEMPORARY TABLE current
+       (item INT UNSIGNED PRIMARY KEY,
+        brand INT UNSIGNED NOT NULL,
+        units INT NOT NULL,
+        amount DECIMAL(9,2) NOT NULL,
+        KEY (brand))
+     SELECT
+            item, 0 brand,
+            SUM(-1 * allocated) units,
             SUM(-1 * allocated * sale_price(txn_line.retail_price,
                                             txn_line.discount_type,
-                                            txn_line.discount)) Total\$dollar
+                                            txn_line.discount)) amount
        FROM txn
        LEFT JOIN txn_line ON txn.id = txn_line.txn
-       LEFT JOIN item ON txn_line.item = item.id
-       LEFT JOIN brand ON item.brand = brand.id
       WHERE type = 'customer'
         AND ($sql_criteria)
         AND filled BETWEEN '$begin' AND '$end' + INTERVAL 1 DAY
-      GROUP BY 1
-      ORDER BY 2";
+        AND item IS NOT NULL
+      GROUP BY 1";
 
-dump_table($db->query($q));
+$db->query($q) or die('Line : ' . __LINE__ . $db->error);
 
-dump_query($q);
+$q= "UPDATE current
+        SET brand = IFNULL((SELECT brand FROM item
+                             WHERE item.id = current.item),
+                           0)";
+
+$db->query($q) or die('Line : ' . __LINE__ . $db->error);
+
+/* Previous */
+$q= "CREATE TEMPORARY TABLE previous
+       (item INT UNSIGNED PRIMARY KEY,
+        brand INT UNSIGNED NOT NULL,
+        units INT NOT NULL,
+        amount DECIMAL(9,2) NOT NULL,
+        KEY (brand))
+     SELECT
+            item, 0 brand,
+            SUM(-1 * allocated) units,
+            SUM(-1 * allocated * sale_price(txn_line.retail_price,
+                                            txn_line.discount_type,
+                                            txn_line.discount)) amount
+       FROM txn
+       LEFT JOIN txn_line ON txn.id = txn_line.txn
+      WHERE type = 'customer'
+        AND ($sql_criteria)
+        AND filled BETWEEN '$begin' - INTERVAL 1 YEAR
+                       AND '$end' + INTERVAL 1 DAY - INTERVAL 1 YEAR
+        AND item IS NOT NULL
+      GROUP BY 1";
+
+$db->query($q) or die('Line : ' . __LINE__ . $db->error);
+
+$q= "UPDATE previous
+        SET brand = IFNULL((SELECT brand FROM item
+                             WHERE item.id = previous.item),
+                           0)";
+
+$db->query($q) or die('Line : ' . __LINE__ . $db->error);
+
+/* Report */
+$q= "SELECT
+            name, slug, 0,
+            (SELECT SUM(amount) FROM current WHERE brand = id)
+              AS current_amount,
+            (SELECT SUM(amount) FROM previous WHERE brand = id)
+              AS previous_amount
+       FROM brand 
+      ORDER BY name";
+
+$r= $db->query($q) or die($db->error);
+
+$cat= "";
+$parent= 0;
 ?>
-  <button id="download" class="btn btn-default">Download</button>
-</div>
-<form id="post-csv" style="display: none"
-      method="post" action="api/encode-tsv.php">
-  <input type="hidden" name="fn" value="brand-sales.tsv">
-  <textarea id="file" name="file"></textarea>
-</form>
+<table class="table table-striped">
+ <thead>
+  <tr>
+   <th>Category</th>
+   <th align="right">Current</th>
+   <th align="right">Previous</th>
+   <th align="right">Change</th>
+ </thead>
+ <tbody>
+<?
+while ($row= $r->fetch_assoc()) {
+  if ($row['parent'] && !$row['previous_amount'] && !$row['current_amount']) {
+    continue;
+  }
+
+  if ($row['previous_amount'] == 0) {
+    $change = 0;
+  } else {
+    $change= (($row['current_amount'] - $row['previous_amount']) / $row['previous_amount']) * 100;
+  }
+?>
+  <tr class="XXX<?=($change < 0) ? 'danger' : ($change > 100) ? 'success' : ''?>">
+   <td><?=$row['parent'] ? ' &nbsp; ' . ashtml($row['name']) : '<b> ' . ashtml($row['name']) . '</b>' ?></td>
+   <td align="right"><?=amount($row['current_amount'])?></td>
+   <td align="right"><?=amount($row['previous_amount'])?></td>
+   <td align="right"><?=sprintf("%.1f%%", $change)?></td>
+  </tr>
+<?}?>
+ </tbody>
+</table>
+
 <?
 foot();
 ?>
@@ -99,21 +175,6 @@ $(function() {
       format: "yyyy-mm-dd",
       todayHighlight: true
   });
-});
-
-$('#download').on('click', function(ev) {
-  var tsv= "Brand\tQuantity\tAvgPrice\tTotal\r\n";
-  $.each($("#results tr"), function (i, row) {
-    if (i > 0) {
-      tsv += $('td:nth(1)', row).text() + "\t" +
-             $('td:nth(2)', row).text() + "\t" +
-             $('td:nth(3)', row).text() + "\t" +
-             $('td:nth(4)', row).text() +
-             "\r\n";
-    }
-  });
-  $("#file").val(tsv);
-  $("#post-csv").submit();
 });
 </script>
 
