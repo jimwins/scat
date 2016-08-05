8c7bb5e1982c57d2ca8f2ae736a5dea7096e2da4
jim winstead <jimw@trainedmonkey.com>
2016-08-05 14:57:38 -0700

    Fix layout of search form

--- report-price-change.php
+++ report-price-change.php
@@ -17,16 +17,20 @@ head("Price Increases @ Scat", true);
 <form id="report-params" class="form-horizontal" role="form"
       action="<?=$_SERVER['PHP_SELF']?>">
   <div class="form-group">
-    <label for="items" class="col-sm-2 control-label">
+    <label for="items" class="col-sm-1 control-label">
       Items
     </label>
-    <div class="col-sm-10">
+    <div class="col-sm-4">
       <input id="items" name="items" type="text"
              class="form-control" style="width: 20em"
              value="<?=ashtml($items)?>">
     </div>
-    <select class="form-control" name="vendor">
-      <option value="">All vendors</option>
+    <label for="vendor" class="col-sm-1 control-label">
+      Vendor
+    </label>
+    <div class="col-sm-4">
+      <select class="form-control" name="vendor">
+        <option value="">All vendors</option>
 <?
 $q= "SELECT id, company FROM person WHERE role = 'vendor' ORDER BY company";
 $r= $db->query($q);
@@ -39,10 +43,9 @@ while ($row= $r->fetch_assoc()) {
        '</option>';
 }
 ?>
-    </select>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
+      </select>
+    </div>
+    <div class="col-sm-2">
       <input type="submit" class="btn btn-primary" value="Show">
     </div>
   </div>
