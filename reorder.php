19c22965a12faa31b0ce32d60d30116efe26e2bb
jim winstead <jimw@trainedmonkey.com>
2016-09-11 12:13:52 -0700

    Don't reorder items not inventoried

--- reorder.php
+++ reorder.php
@@ -94,7 +94,7 @@ $q= "SELECT meta, Code\$item, Name, Stock\$right,
                       AS Order\$order
                FROM item
                LEFT JOIN txn_line ON (item = item.id)
-              WHERE active AND NOT deleted
+              WHERE inventoried AND active AND NOT deleted
                 $extra
               GROUP BY item.id
               ORDER BY code) t
