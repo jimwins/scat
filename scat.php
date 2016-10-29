b4b26e920dd99c9f709c0013808b5f4da24c7baf
jim winstead <jimw@trainedmonkey.com>
2016-10-29 12:15:30 -0700

    Add jquery.udraggable and dependency

--- scat.php
+++ scat.php
@@ -55,6 +55,8 @@ header("content-type: text/html;charset=utf-8");?>
  <script src="js/jquery.html5uploader.js"></script>
  <script src="js/jquery.jeditable.js"></script>
  <script src="js/jquery.dragbetter.js"></script>
+ <script src="js/jquery.event.ue.js"></script>
+ <script src="js/jquery.udraggable.js"></script>
 <?if (!$allnew) {?>
  <script src="js/jquery-ui.min.js"></script>
  <script src="js/jquery.simplemodal.1.4.4.min.js"></script>
