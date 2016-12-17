87bb01f860ae51220b3744b6b12f0622ea2550d2
jim winstead <jimw@trainedmonkey.com>
2016-12-16 18:05:22 -0800

    Add timeout when connecting/sending to pole

--- lib/pole.php
+++ lib/pole.php
@@ -2,6 +2,9 @@
 
 function pole_display_price($label, $price) {
   $sock= socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+  /* Set 1 sec timeout to avoid getting stuck, should be plenty long enough */
+  socket_set_option($sock, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 1,
+                                                          'usec' => 0));
   if (@socket_connect($sock, '127.0.0.1', 1888)) {
     socket_write($sock,
                  sprintf("\x1f\x0d\x0a%-19.19s\x0a\x0d$%18.2f ",
