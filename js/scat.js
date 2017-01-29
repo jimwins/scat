36c6617894c76be6481a92b5b53d1f06d9894eee
jim winstead <jimw@trainedmonkey.com>
2017-01-28 19:00:01 -0800

    Allow title of modal to be specified

--- js/scat.js
+++ js/scat.js
@@ -130,7 +130,8 @@ function displayError(data) {
   }
 
   if (!$('body').hasClass('modal-open')) {
-    var modal= $('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">Error</h4></div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>');
+    var title= data.title ? data.title : 'Error';
+    var modal= $('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button><h4 class="modal-title">' + title + '</h4></div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div>');
 
     $('.modal-body', modal).prepend(data.error);
 
