05facc66c1eb2cce0eaf462b4fa9dd0bd909aaf1
jim winstead <jimw@trainedmonkey.com>
2017-02-08 13:42:16 -0800

    Handle empty values on person better (force to NULL)

--- api/person-update.php
+++ api/person-update.php
@@ -9,15 +9,19 @@ $person= person_load($db, $person_id);
 if (!$person)
   die_jsonp('No such person.');
 
-if ($_REQUEST['phone']) {
+if (isset($_REQUEST['phone'])) {
   $_REQUEST['loyalty_number']= preg_replace('/[^\d]/', '', $_REQUEST['phone']);
 }
+
+// TODO: reset sms_ok and email_ok when email or phone changed
 
 foreach (array('name', 'role', 'company', 'email', 'notes',
                'phone', 'loyalty_number', 'tax_id', 'address') as $key) {
   if (isset($_REQUEST[$key])) {
-    $value= $db->real_escape_string($_REQUEST[$key]);
-    $q= "UPDATE person SET $key = '$value' WHERE id = $person_id";
+    $value= trim($_REQUEST[$key]);
+    /* Turn empty strings into NULL, escape others and wrap in quotes */
+    $value= ($value != '') ?  "'" . $db->escape($value) . "'" : 'NULL';
+    $q= "UPDATE person SET $key = $value WHERE id = $person_id";
 
     $r= $db->query($q)
       or die_query($db, $q);
