d1a42145d8b78d2f7767a182f2707569bae70888
jim winstead <jimw@trainedmonkey.com>
2017-12-18 12:36:02 -0800

    Fix use of card_id field (rename from old card field)

--- api/giftcard-check-balance.php
+++ api/giftcard-check-balance.php
@@ -1,7 +1,7 @@
 <?php
 include '../scat.php';
 
-$card= $db->escape($_GET['card']);
+$card= $db->escape($_REQUEST['card']);
 $card= preg_replace('/^RAW-/', '', $card);
 
 $q= "SELECT id, active, CONCAT(id, pin) card
@@ -19,12 +19,12 @@ $full_card= $row[2];
 
 # card is active, now check the balance!
 
-$q= "SELECT CONCAT(card, pin) AS card,
+$q= "SELECT CONCAT(card_id, pin) AS card,
             DATE_FORMAT(MAX(entered), '%W, %M %e, %Y') AS latest,
             SUM(amount) AS balance
        FROM giftcard_txn JOIN giftcard ON (giftcard.id = card_id)
-      WHERE card = '$card'
-      GROUP BY card";
+      WHERE card_id = '$card'
+      GROUP BY card_id";
 $r= $db->query($q);
 if (!$r) die(jsonp(array("error" => "Unable to check balance.",
                          "detail" => $db->error)));
