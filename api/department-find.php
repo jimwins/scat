fc777333d29f3d88ff3054a7ea5b3bb1cb58919d
jim winstead <jimw@trainedmonkey.com>
2018-01-13 18:39:45 -0800

    API for getting departments by parent

---
+++ api/department-find.php
@@ -0,0 +1,14 @@
+<?php
+require '../scat.php';
+require '../lib/catalog.php';
+
+$parent= (int)$_REQUEST['parent'];
+
+$departments= Model::factory('Department')
+                ->where('parent_id', $parent)
+                ->find_many();
+
+if (!$departments)
+  die_jsonp(array('error' => 'No departments in that department!'));
+
+echo jsonp(array_map(function ($d) { return $d->as_array(); }, $departments));
