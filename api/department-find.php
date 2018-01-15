82572e8b07dfa67388f4422b3d89ddc31b1da96b
jim winstead <jimw@trainedmonkey.com>
2018-01-14 21:00:47 -0800

    Return second level of departments on request

--- api/department-find.php
+++ api/department-find.php
@@ -12,4 +12,14 @@ $departments= Model::factory('Department')
 if (!$departments)
   die_jsonp(array('error' => 'No departments in that department!'));
 
+if ((int)$_REQUEST['levels'] == 2) {
+  $departments= array_map(function ($d) {
+                            $d->sub= Model::factory('Department')
+                                       ->where('parent_id', $d->id)
+                                       ->order_by_asc('name')
+                                       ->find_array();
+                            return $d;
+                          }, $departments);
+}
+
 echo jsonp(array_map(function ($d) { return $d->as_array(); }, $departments));
