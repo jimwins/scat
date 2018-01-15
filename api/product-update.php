5aca1e88660762a1796d9bf28e532d17d6a457f7
jim winstead <jimw@trainedmonkey.com>
2018-01-14 21:02:12 -0800

    Handle updates of all product fields
    Doesn’t record renames yet for redirects

--- api/product-update.php
+++ api/product-update.php
@@ -8,13 +8,30 @@ $slug= $_REQUEST['slug'];
 
 try {
   $product= Model::factory('Product')->find_one($id);
-  if ($name)
-    $product->name= $name;
-  if ($slug)
-    $product->slug= $slug;
-  if (array_key_exists('active', $_REQUEST))
-    $product->active= (int)$_REQUEST['active'];
+
+  $old_slug= "";
+  if (($product->slug && $_REQUEST['slug'] != $product->slug) ||
+      ($product->department_id &&
+       $_REQUEST['department_id'] != $product->department_id))
+  {
+    // XXX handle rename
+    $old_slug= '';
+  }
+
+  foreach ($_REQUEST as $k => $v) {
+    if (in_array($k, array('department_id', 'brand_id', 'name', 'description',
+                           'slug', 'image', 'variation_style', 'active')))
+    {
+      $product->set($k, $v);
+    }
+  }
+
   $product->save();
+
+  // XXX Save redirect information
+  if ($old_slug) {
+  }
+
 } catch (\PDOException $e) {
   die_jsonp($e->getMessage());
 }
