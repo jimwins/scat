ea1c79b2af4008ca7e3fc94fad0187dd427ba94b
jim winstead <jimw@trainedmonkey.com>
2018-01-19 14:44:24 -0800

    Pass through page size, return total number of rows

--- api/person-load-activity.php
+++ api/person-load-activity.php
@@ -4,10 +4,14 @@ include '../lib/person.php';
 
 $person_id= (int)$_REQUEST['person'];
 $page= (int)$_REQUEST['page'];
+$limit= isset($_REQUEST['limit']) ? (int)$_REQUEST['limit'] : 50;
 
-$activity= person_load_activity($db, $person_id, $page);
+$activity= person_load_activity($db, $person_id, $page, $limit);
+
+$rows= $db->get_one('SELECT FOUND_ROWS()');
 
 if (!is_array($activity))
   die_jsonp('No such person.');
 
-echo jsonp(array('activity' => $activity, 'activity_page' => $page));
+echo jsonp(array('activity' => $activity, 'activity_page' => $page,
+                 'total' => $rows));
