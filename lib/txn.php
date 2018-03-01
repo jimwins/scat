45fa66fbe4f129ff872b06accb284e1aa9ca987c
jim winstead <jimw@trainedmonkey.com>
2018-02-28 17:08:57 -0800

    Allow price override by product id

--- lib/txn.php
+++ lib/txn.php
@@ -201,11 +201,16 @@ function txn_apply_discounts($db, $id) {
     or die_query($db, $q);
 
   foreach ($discounts as $d) {
+    if ($d['pattern_type'] == 'product') {
+      $condition= "product_id = '{$d['pattern']}'";
+    } else {
+      $condition= "code {$d['pattern_type']} '{$d['pattern']}'";
+    }
     $count= $db->get_one("SELECT ABS(SUM(ordered))
                             FROM txn_line
                             JOIN item ON txn_line.item = item.id
                            WHERE txn = $id
-                             AND code {$d['pattern_type']} '{$d['pattern']}'
+                             AND $condition
                              AND NOT discount_manual");
 
     if (!$count) {
@@ -233,14 +238,14 @@ function txn_apply_discounts($db, $id) {
               SET txn_line.discount = $new_discount,
                   txn_line.discount_type = '$new_discount_type'
             WHERE txn = $id AND txn_line.item = item.id
-              AND code {$d['pattern_type']} '{$d['pattern']}'
+              AND $condition
               AND NOT discount_manual";
     } else {
       $q= "UPDATE txn_line, item
               SET txn_line.discount = item.discount,
                   txn_line.discount_type = item.discount_type 
             WHERE txn = $id AND txn_line.item = item.id
-              AND code {$d['pattern_type']} '{$d['pattern']}'
+              AND $condition
               AND NOT discount_manual";
     }
 
