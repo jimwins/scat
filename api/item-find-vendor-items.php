2b9a60a9de1f8ea7c7fa715ec592bf7ab40a446a
jim winstead <jimw@trainedmonkey.com>
2018-07-16 12:50:21 -0700

    Match by item code, too

--- api/item-find-vendor-items.php
+++ api/item-find-vendor-items.php
@@ -6,6 +6,14 @@ $id= (int)$_REQUEST['id'];
 if (!$id)
   die_jsonp("You need to supply an item.");
 
+$q= "UPDATE vendor_item, item
+        SET vendor_item.item = item.id
+      WHERE vendor_item.item = 0 AND vendor_item.code = item.code
+        AND item.id = $id";
+
+$r= $db->query($q)
+  or die_query($db, $q);
+
 $q= "UPDATE vendor_item, barcode
         SET vendor_item.item = barcode.item
       WHERE vendor_item.item = 0 AND vendor_item.barcode = barcode.code
