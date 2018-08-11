1ea2ed043e46d280cc71dd7f4d6e78e78c376f5e
jim winstead <jimw@trainedmonkey.com>
2018-08-10 18:42:45 -0700

    Remove redundant printing code

--- print/gift-card.php
+++ print/gift-card.php
@@ -1,55 +1,20 @@
 <?php
 
 include '../scat.php';
-
-include '../lib/fpdf/fpdf.php';
-include '../lib/fpdi/fpdi.php';
-include '../lib/php-barcode.php';
+include '../lib/giftcard.php';
 
 $card= $_REQUEST['card'];
-$balance= $_REQUEST['balance'];
-$issued= $_REQUEST['issued'];
 
-$issued= (new \Datetime($issued))->format('l, F j, Y');
+$id= substr($card, 0, 7);
+$pin= substr($card, -4);
 
-// initiate FPDI
-$pdf = new FPDI('P', 'in', array(8.5, 11));
-// add a page
-$pdf->AddPage();
-// set the source file
-$pdf->setSourceFile("blank-gift-card.pdf");
-// import page 1
-$tplIdx = $pdf->importPage(1);
-// use the imported page
-$pdf->useTemplate($tplIdx);
+$card= Model::factory('Giftcard')
+         ->where('id', $id)
+         ->where('pin', $pin)
+         ->find_one();
 
-// now write some text above the imported page
-$pdf->SetFont('Helvetica');
-$pdf->SetTextColor(0, 0, 0);
-$pdf->SetFontSize(($basefontsize= 18));
-
-if ($balance) {
-  $width= $pdf->GetStringWidth('$' . $balance);
-  $pdf->SetXY(4.25 - ($width / 2), 2.5);
-  $pdf->Write(0, '$' . $balance);
+if (!$card) {
+  die_jsonp(array("error" => "Unable to find info for that card."));
 }
 
-$width= $pdf->GetStringWidth($issued);
-$pdf->SetXY(4.25 - ($width / 2), 3.25);
-$pdf->Write(0, $issued);
-
-$code= "RAW-$card";
-$type= "code39";
-
-Barcode::fpdf($pdf, '000000',
-              4.25, 5,
-              0 /* angle */, $type,
-              $code, (1/72), $basefontsize/2/72);
-
-$pdf->SetFontSize(10);
-$width= $pdf->GetStringWidth($card);
-$pdf->SetXY(4.25 - ($width / 2), 5.2);
-$pdf->Write(0, $card);
-
-$pdf->Output();
-
+echo $card->getPDF();
