b0bad347a83aaec1a94db71fa7235d03e9d12676
jim winstead <jimw@trainedmonkey.com>
2019-01-17 11:21:12 -0800

    Get timezone from environment

--- scat.php
+++ scat.php
@@ -1,7 +1,7 @@
 <?
 error_reporting(E_ALL & ~E_NOTICE);
 
-date_default_timezone_set('America/Los_Angeles');
+date_default_timezone_set($_ENV['PHP_TIMEZONE'] ?: $_ENV['TZ']);
 
 if (get_magic_quotes_gpc())
   die("Sorry, you need to disable magic quotes for Scat to work.");
