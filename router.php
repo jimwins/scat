f376bca88c0b56311bfcba43036de1b6c6b1c0ed
jim winstead <jimw@trainedmonkey.com>
2019-02-18 17:02:23 -0800

    Add connection to Sphinx

--- router.php
+++ router.php
@@ -23,6 +23,11 @@ $config= [
     'user' => DB_USER,
     'password' => DB_PASSWORD,
     'dbname' => DB_SCHEMA
+  ],
+  'search' => [
+    'dsn' => SEARCH_DSN,
+    'user' => SEARCH_USER,
+    'password' => SEARCH_PASSWORD
   ]
 ];
 
@@ -64,6 +69,15 @@ $container['db']= function ($c) {
   $db= $c['settings']['db'];
   $pdo= new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
                 $db['user'], $db['pass']);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  return $pdo;
+};
+
+/* Search */
+$container['search']= function ($c) {
+  $db= $c['settings']['search'];
+  $pdo= new PDO($db['dsn'], $db['user'], $db['pass']);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   return $pdo;
