182cc1993026b62369c392973b4860df695dd780
jim winstead <jimw@trainedmonkey.com>
2019-02-23 11:56:11 -0800

    Move stock and sale_price into Item methods

--- lib/Scat/Item.php
+++ lib/Scat/Item.php
@@ -14,6 +14,30 @@ class Item extends \Model {
   public function barcodes() {
     return $this->has_many('Barcode', 'item');
   }
+
+  public function sale_price() {
+    switch ($this->discount_type) {
+    case 'percentage':
+      // TODO fix rounding
+      return bcmul($this->retail_price,
+                   bcdiv(bcsub(100, $this->discount),
+                         100));
+    case 'relative':
+      return bcsub($this->retail_price, $this->discount);
+    case 'fixed':
+      return $this->discount;
+    case '':
+    case null:
+      return $this->retail_price;
+    default:
+      throw new Exception('Did not understand discount for item.');
+    }
+  }
+
+  public function stock() {
+    return $this->has_many('TxnLine', 'item')
+                ->sum('allocated') ?: 0;
+  }
 }
 
 class Barcode extends \Model {
@@ -21,4 +45,3 @@ class Barcode extends \Model {
     return $this->belongs_to('Item', 'item');
   }
 }
-
--- lib/Scat/Product.php
+++ lib/Scat/Product.php
@@ -13,14 +13,6 @@ class Product extends \Model implements \JsonSerializable {
   public function items($only_active= true) {
     return $this->has_many('Item')
                 ->select('item.*')
-                ->select_expr('sale_price(item.retail_price,
-                                          item.discount_type,
-                                          item.discount)',
-                              'sale_price')
-                ->select_expr('(SELECT IFNULL(SUM(allocated),0)
-                                  FROM txn_line
-                                 WHERE txn_line.item = item.id)',
-                              'stock')
                 ->where_gte('active', (int)$only_active);
   }
 
