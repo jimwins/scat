e12d4b901f0aa1e3e7160326dd6bf324e6f523fb
jim winstead <jimw@trainedmonkey.com>
2019-03-19 20:19:18 -0700

    Upgrade from PHPExcel to PhpSpreadsheet

--- api/encode-xls.php
+++ api/encode-xls.php
@@ -6,7 +6,7 @@ if (preg_match('/^([-A-Za-z0-9_])+\\.([A-Za-z0-9]+)$/', $_REQUEST['fn'])) {
   $fn= $_REQUEST['fn'];
 }
 
-$xls= new PHPExcel();
+$xls= new \PhpOffice\PhpSpreadsheet\Spreadsheet();
 
 $xls->setActiveSheetIndex(0);
 $row= 1;
@@ -15,9 +15,9 @@ $lines= explode("\r\n", urldecode($_REQUEST['file']));
 
 foreach ($lines as $line) {
   list($code, $qty)= explode("\t", $line);
-  $xls->getActiveSheet()->setCellValueByColumnAndRow(0, $row, $code)
-                        ->setCellValueByColumnAndRow(1, $row, "")
-                        ->setCellValueByColumnAndRow(2, $row, $qty);
+  $xls->getActiveSheet()->setCellValueByColumnAndRow(1, $row, $code)
+                        ->setCellValueByColumnAndRow(2, $row, "")
+                        ->setCellValueByColumnAndRow(3, $row, $qty);
   $row+=1;
 }
 
@@ -25,5 +25,5 @@ header('Content-Type: application/vnd.ms-excel');
 header('Content-Disposition: attachment; filename="' . $fn . '"'); 
 header('Cache-Control: max-age=0');
 
-$objWriter = PHPExcel_IOFactory::createWriter($xls, 'Excel5');
+$objWriter = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($xls, 'Xls');
 $objWriter->save('php://output');
