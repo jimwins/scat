404c896e9a1928a200e8e01e8200a582eedcd652
jim winstead <jimw@trainedmonkey.com>
2019-04-20 12:20:26 -0700

    Make Note model JSON serializable

--- lib/Scat/Note.php
+++ lib/Scat/Note.php
@@ -1,7 +1,7 @@
 <?php
 namespace Scat;
 
-class Note extends \Model {
+class Note extends \Model implements \JsonSerializable {
   public function txn() {
     if ($this->kind == 'txn') {
       return $this->belongs_to('Txn', 'attach_id')->find_one();
@@ -14,5 +14,9 @@ class Note extends \Model {
 
   public function parent() {
     return $this->belongs_to('Note', 'parent_id');
+  }
+
+  public function jsonSerialize() {
+    return $this->as_array();
   }
 }
