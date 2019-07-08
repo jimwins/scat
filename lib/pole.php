24ea41d66a89936bac550a36a4cdff7aabdb6f24
jim winstead <jimw@trainedmonkey.com>
2019-07-08 15:33:30 -0700

    Output pole price for debugging

--- lib/pole.php
+++ lib/pole.php
@@ -1,6 +1,10 @@
 <?php
 
 function pole_display_price($label, $price) {
+  if ($GLOBALS['DEBUG']) {
+    error_log(sprintf("POLE: %-19.19s - $%18.2f", $label, $price));
+  }
+
   if (!defined('POLE_SERVER')) {
     error_log("POLE_SERVER not configured");
     return;
