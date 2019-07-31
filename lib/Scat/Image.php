e113cb839a69d86d6253e583604eae421bf83185
jim winstead <jimw@trainedmonkey.com>
2019-07-31 12:41:27 -0700

    Fix upload via URL (need to refactor this)

--- lib/Scat/Image.php
+++ lib/Scat/Image.php
@@ -14,7 +14,7 @@ class Image extends \Model implements \JsonSerializable {
     return '/i/md/' . $this->uuid . '.jpg';
   }
 
-  public function regenerate() {
+  public function regenerate($original= null) {
     $client= new \GuzzleHttp\Client();
 
     $b2= new \ChrisWhite\B2\Client(B2_ACCOUNT_ID, B2_APPLICATION_KEY);
@@ -25,8 +25,10 @@ class Image extends \Model implements \JsonSerializable {
     ]);
 
     // XXX must be a better way to do this, but this works for now
-    $original= 'https:' . ORDURE_STATIC . $this->original();
-    error_log("original: $original");
+    if (!$original) {
+      $original= 'https:' . ORDURE_STATIC . $this->original();
+      error_log("original: $original");
+    }
 
     // Generate the standard size image (max 768px)
     $res= $short->reduceUrl($original, [
@@ -78,20 +80,61 @@ class Image extends \Model implements \JsonSerializable {
       'FileName' => '/i/th/' . $this->uuid . '.jpg',
       'Body' => $file
     ]);
+
+    return $this;
   }
 
   public static function createFromUrl($url) {
     error_log("Creating image from URL '$url'");
-    $client= new \GuzzleHttp\Client();
-    $res= $client->get($url);
-    $file= $res->getBody();
     $name= basename(parse_url($url, PHP_URL_PATH));
 
-    if (!$file) {
-      throw new \Exception("Unable to load from $url");
+    $ext= pathinfo($name, PATHINFO_EXTENSION);
+
+    // Could use real UUID() but this is shorter. Hardcoded '1' could be
+    // replaced with a server-id to further avoid collisions
+    $uuid= sprintf("%08x%02x%s", time(), 1, bin2hex(random_bytes(8)));
+
+    // Make debug uploads easier to find
+    if ($GLOBALS['DEBUG']) {
+      $uuid= "DEBUG" . substr($uuid, 5);
     }
 
-    return self::createFromFile($file, $name);
+    $short= new \Scat\ShortPixel(SHORTPIXEL_KEY, [
+      'convertto' => 'jpg',
+      'wait' => 30
+    ]);
+
+    // Just "glossy" compression on the original
+    $res= $short->reduceUrl($url, [ 'lossy' => 2 ]);
+
+    if ($res->LossyURL && $res->Status->Code == 2) {
+      $original= $res->OriginalURL;
+      $lossy= $res->LossyURL;
+    }
+
+    // Upload the glossy as the original
+    $b2= new \ChrisWhite\B2\Client(B2_ACCOUNT_ID, B2_APPLICATION_KEY);
+
+    $client= new \GuzzleHttp\Client();
+    $res= $client->get($lossy);
+    $orig_file= $res->getBody();
+
+    $upload= $b2->upload([
+      'BucketName' => B2_BUCKET,
+      'FileName' => '/i/o/' . $uuid . '.' . $ext,
+      'Body' => $orig_file
+    ]);
+
+    // Save the details
+    $image= \Model::factory('Image')->create();
+    $image->uuid= $uuid;
+    //$image->width= $img->width();
+    //$image->height= $img->height();
+    $image->ext= $ext;
+    $image->name= $name;
+    $image->save();
+
+    return $image->regenerate($original);
   }
 
   public static function createFromPath($fn) {
