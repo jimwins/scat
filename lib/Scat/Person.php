2ba2c9efc2f701d0e8f8be14c62111fb707dc468
jim winstead <jimw@trainedmonkey.com>
2019-09-28 13:20:24 -0700

    Add find() static method

--- lib/Scat/Person.php
+++ lib/Scat/Person.php
@@ -38,4 +38,45 @@ class Person extends \Model implements \JsonSerializable {
   public function jsonSerialize() {
     return $this->asArray();
   }
+
+  static function find($q, $all= false) {
+    $criteria= [];
+
+    $terms= preg_split('/\s+/', trim($q));
+    foreach ($terms as $term) {
+      if (preg_match('/id:(\d*)/', $term, $m)) {
+        $id= (int)$m[1];
+        $criteria[]= "(person.id = $id)";
+        $all= true;
+      } else if (preg_match('/role:(.+)/', $term, $m)) {
+        $role= addslashes($m[1]);
+        $criteria[]= "(person.role = '$role')";
+      } elseif (preg_match('/^active:(.+)/i', $term, $dbt)) {
+        $criteria[]= $dbt[1] ? "(person.active)" : "(NOT person.active)";
+        $all= true;
+      } else {
+        $term= addslashes($term);
+        $criteria[]= "(person.name LIKE '%$term%'
+                   OR person.company LIKE '%$term%'
+                   OR person.email LIKE '%$term%'
+                   OR person.loyalty_number LIKE '%$term%'
+                   OR person.phone LIKE '%$term%'
+                   OR person.notes LIKE '%$term%')";
+      }
+    }
+
+    $sql_criteria= join(' AND ', $criteria);
+
+    $people= \Model::factory('Person')->select('person.*')
+                                      ->where_raw($sql_criteria)
+                                      ->where_gte('person.active',
+                                                  $all ? 0 : 1)
+                                      ->where_not_equal('person.deleted', 1)
+                                      ->order_by_asc('company')
+                                      ->order_by_asc('name')
+                                      ->order_by_asc('loyalty_number')
+                                      ->find_many();
+
+    return $people;
+  }
 }
