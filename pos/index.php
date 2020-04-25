b8d20b6f82f5f5229301f90e58b5a2a3310803ea
jim winstead <jimw@trainedmonkey.com>
2020-04-25 10:53:44 -0700

    Don't need pdo service (accessed through ORM)

--- pos/index.php
+++ pos/index.php
@@ -47,16 +47,6 @@ $container['cache']= function () {
   return new \Slim\HttpCache\CacheProvider();
 };
 
-/* PDO */
-$container['db']= function ($c) {
-  $db= $c['settings']['db'];
-  $pdo= new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
-                $db['user'], $db['pass']);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  return $pdo;
-};
-
 /* Twig for templating */
 $container['view']= function ($container) {
   $view= new \Slim\Views\Twig('../ui', [
