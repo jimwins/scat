4943c31e9a5ec3d2e8cfaba2fc9772c02592d20c
jim winstead <jimw@trainedmonkey.com>
2020-04-28 17:01:24 -0700

    Add 'forget' method to Config service

--- lib/Scat/Service/Config.php
+++ lib/Scat/Service/Config.php
@@ -10,4 +10,8 @@ class Config
   public function set($name, $value) {
     return \Scat\Model\Config::setValue($name, $value);
   }
+
+  public function forget($name) {
+    return \Scat\Model\Config::forgetValue($name, $value);
+  }
 }
