0adfb5dda7ab089674519871734a1e593729bb76
jim winstead <jimw@trainedmonkey.com>
2021-04-18 11:04:17 -0700

    Make scat.dialog return a Promise semi-correctly

--- js/scat.main.js
+++ js/scat.main.js
@@ -35,31 +35,34 @@ class ScatUtils {
     })
     .then((res) => { return res.text() })
     .then((text) => {
-      console.log("Loaded '" + url + "'.")
-      let modal= this.htmlToElement(text)
-      document.body.insertAdjacentElement('beforeend', modal)
-      $(modal).on('show.bs.modal', function(e) {
-        // Re-inject the script to get it to execute
-        let code= this.getElementsByTagName('script')[0].innerHTML
-        let script= document.createElement('script')
-        script.modal= this
-        script.appendChild(document.createTextNode(code))
-        this.appendChild(script).parentNode.removeChild(script)
-        /* Attach dialog to each object with event handler */
-        this.querySelectorAll('*').forEach((el) => {
-          if (typeof el.onclick === 'function' ||
-              typeof el.onsubmit === 'function') {
-            el.dialog= this
-          }
+      return new Promise((resolve, reject) => {
+        let modal= this.htmlToElement(text)
+        document.body.insertAdjacentElement('beforeend', modal)
+        $(modal).on('show.bs.modal', function(e) {
+          // Re-inject the script to get it to execute
+          let code= this.getElementsByTagName('script')[0].innerHTML
+          let script= document.createElement('script')
+          script.modal= this
+          script.appendChild(document.createTextNode(code))
+          this.appendChild(script).parentNode.removeChild(script)
+          /* Attach dialog to each object with event handler */
+          this.querySelectorAll('*').forEach((el) => {
+            if (typeof el.onclick === 'function' ||
+                typeof el.onsubmit === 'function') {
+              el.dialog= this
+            }
+          })
         })
+        $(modal).on('hidden.bs.modal', function(e) {
+          $(this.script).remove()
+          $(this).remove()
+          resolve()
+        });
+        $(modal).modal()
+        from.disabled= false
+      }).finally(() => {
+        from.disabled= false
       })
-      $(modal).on('hidden.bs.modal', function(e) {
-        $(this.script).remove()
-        $(this).remove()
-      });
-      $(modal).modal()
-      from.disabled= false
-      return Promise.resolve($(modal))
     })
   }
 
