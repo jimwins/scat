f104ac62acebcc9e8b72d75e2a7845d7649e1a5f
jim winstead <jimw@trainedmonkey.com>
2021-06-01 14:53:46 -0700

    Fix tracing, but catch exceptions to avoid issues

--- lib/Scat/Service/Dejavoo.php
+++ lib/Scat/Service/Dejavoo.php
@@ -32,12 +32,20 @@ class Dejavoo {
       $captured= bcmul($captured, -1);
     }
 
-    $trace= $data->factory('CcTrace')->create();
-    $trace->txn_id= $txn->id;
-    $trace->raw_request= $cc->raw_request;
-    $trace->raw_response= $cc->raw_response;
-    $trace->raw_curlinfo= $cc->raw_curlinfo;
-    $trace->save();
+    /*
+     * We want to record the trace, but we ignore any errors because
+     * we don't want to roll back the payment if it has a problem.
+     */
+    try {
+      $trace= $this->data->factory('CcTrace')->create();
+      $trace->txn_id= $txn->id;
+      $trace->raw_request= $cc->raw_request;
+      $trace->raw_response= $cc->raw_response;
+      $trace->raw_curlinfo= $cc->raw_curlinfo;
+      $trace->save();
+    } catch (\Exception $e) {
+      error_log("Failed to capture trace: " . $e->getMessage());
+    }
 
     return [
       'amount' => $captured,
