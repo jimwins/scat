39e9a75545a5ba59a6745ea84f7398f02e29a082
jim winstead <jimw@trainedmonkey.com>
2021-06-15 10:52:23 -0700

    Add replaceChildren polyfill

--- js/polyfills.js
+++ js/polyfills.js
@@ -31,3 +31,24 @@ if (!Object.fromEntries) {
     },
   });
 }
+
+/**
+* replaceChildren.js
+* @mdn https://developer.mozilla.org/en-US/docs/Web/API/ParentNode/replaceChildren
+* @email yanwenbin1991@live.com
+**/
+(function (item) {
+    if (!item.replaceChildren) {
+        item.replaceChildren = function () {
+            var parentNode = this;
+            var oldNodes = [].slice.call(parentNode.childNodes);
+            var newNodes = [].slice.call(arguments);
+            oldNodes.forEach(function (nodes) {
+                if (!newNodes.find(function(el){ return el === nodes})) {
+                    parentNode.removeChild(nodes)
+                }
+            });
+            parentNode.append.apply(this,newNodes);
+        }
+    }
+})(HTMLElement.prototype);
