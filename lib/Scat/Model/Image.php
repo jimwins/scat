174fbf056abc2a4282179981d473edfcab2a2972
jim winstead <jimw@trainedmonkey.com>
2021-09-24 10:28:24 -0700

    Play with using Gumlet for images

--- lib/Scat/Model/Image.php
+++ lib/Scat/Model/Image.php
@@ -3,25 +3,26 @@ namespace Scat\Model;
 
 class Image extends \Scat\Model {
   public function original() {
-    return PUBLITIO_BASE . '/file/' . $this->uuid . '.' . $this->ext;
+    return GUMLET_BASE .
+           $this->uuid . '.' . $this->ext;
   }
 
   public function thumbnail() {
-    return PUBLITIO_BASE .
-           '/file/w_128,h_128,c_fit' .
-           '/' . $this->uuid . '.jpg';
+    return GUMLET_BASE .
+           $this->uuid . '.' . $this->ext .
+           '?w=128&h=128&mode=fit&fm=auto';
   }
 
   public function medium() {
-    return PUBLITIO_BASE .
-           '/file/w_384,h_384,c_fit' .
-           '/' . $this->uuid . '.jpg';
+    return GUMLET_BASE .
+           $this->uuid . '.' . $this->ext .
+           '?w=384&h=384&mode=fit&fm=auto';
   }
 
   public function large_square() {
-    return PUBLITIO_BASE .
-           '/file/w_1024,h_1024,c_fill' .
-           '/' . $this->uuid . '.jpg';
+    return GUMLET_BASE .
+           $this->uuid . '.' . $this->ext .
+           '?w=1024&h=1024&mode=fill&fm=auto';
   }
 
   public function productsUsedBy() {
