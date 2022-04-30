4d5d41bcdf10af165b4ee785461343e0356885b2
jim winstead <jimw@trainedmonkey.com>
2022-04-30 10:22:08 -0700

    Add clarifying comment

--- lib/Scat/Model/Item.php
+++ lib/Scat/Model/Item.php
@@ -60,6 +60,7 @@ class Item extends \Scat\Model {
     // 9x12 Canvas -> 9" x 12" Canvas
     $title= preg_replace('/^([-\\/\d.]+)x([-\\/\d.]+) /', '\1" x \2" ', $title);
     // 9x12 Sketch 80/WB/90# -> 9" x 12" Sketch (80 sheets, Wirebound, 90 lbs.)
+    // note: has to be at the end of the name.
     $title= preg_replace_callback(
       '!(\d+)/(\w\w\w?)(?:/([\d.]+)(gsm|#)(\w\w?)?)?$!',
       "self::expand_pad_details",
