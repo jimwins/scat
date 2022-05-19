236b6881d8e7f43e40fd509b7681a94688c86997
jim winstead <jimw@trainedmonkey.com>
2022-05-19 13:36:34 -0700

    Add Google Distance Matrix API to Google service

--- lib/Scat/Service/Google.php
+++ lib/Scat/Service/Google.php
@@ -3,11 +3,12 @@ namespace Scat\Service;
 
 class Google
 {
-  private $service_key, $merchant_center_id;
+  private $service_key, $merchant_center_id, $maps_api_key;
 
   public function __construct(Config $config) {
     $this->service_key= $config->get('google.service_key');
     $this->merchant_center_id= $config->get('google.merchant_center_id');
+    $this->maps_api_key= $config->get('google.maps_api_key');
 
     if ($this->service_key) {
       $this->service_key= json_decode($this->service_key, true);
@@ -41,6 +42,29 @@ class Google
     $req->setQuery($query);
 
     return $service->reports->search(17327991, $req)->getResults();
+  }
+
+  public function getMapsDistanceMatrix($from, $to) {
+    try {
+      $gm= new \yidas\googleMaps\Client([ 'key' => $this->maps_api_key ]);
+
+      $result= $gm->distanceMatrix($from, $to);
+
+      if ($result['status'] == 'OK') {
+        return [
+          // we want distance in miles
+          $result['rows'][0]['elements'][0]['distance']['value'] / 1609.34,
+          // and time in minutes
+          $result['rows'][0]['elements'][0]['duration']['value'] / 60,
+        ];
+      } else {
+        error_log("Not OK: " . json_encode($result));
+      }
+    } catch (\Exception $e) {
+      error_log("Google exception: {$e->getMessage()}\n");
+    }
+
+    return [ 0, 0 ];
   }
 
 }
