bc1562872be4ebf9c2fe66c0cdd8c009f7694d94
jim winstead <jimw@trainedmonkey.com>
2022-05-27 13:12:53 -0700

    Add dummy zaraz script for development

---
+++ static/js/dummy-zaraz.js
@@ -0,0 +1,18 @@
+/* Just a simple Zaraz dummy that spits stuff to console */
+"use strict";
+
+class Zaraz {
+  set (key, value, options= null) {
+    console.log("Zaraz Set: %s = %s %O", key, value, options)
+  }
+
+  track (event, parameters= null) {
+    console.log("Zaraz Track: %s %O", event, parameters)
+  }
+
+  ecommerce (event, parameters) {
+    console.log("Zaraz Ecommerce: %s %O", event, parameters)
+  }
+}
+
+let zaraz= new Zaraz()
