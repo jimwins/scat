18b5ea36ea0b9a745d5cea32f48d9962e3eb3d03
jim winstead <jimw@trainedmonkey.com>
2022-06-14 14:30:26 -0700

    Try to be better about serving correct image size

--- lib/Scat/Model/Image.php
+++ lib/Scat/Model/Image.php
@@ -30,6 +30,21 @@ class Image extends \Scat\Model {
            '?w=1024&h=1024&mode=fill&fill=solid&fill-color=white&fm=' . $fm;
   }
 
+  public function at_size($width, $height= null) {
+    if ($height === null) {
+      if ($this->width) {
+        $height= (int)($this->height / $this->width * $width);
+      } else {
+        $height= $width;
+      }
+    }
+    $fm= in_array(strtolower($this->ext), [ 'tif', 'tiff' ]) ? 'jpeg' : 'auto';
+    $config= $GLOBALS['container']->get(\Scat\Service\Config::class);
+    return $config->get('gumlet.base_url') .
+           $this->uuid . '.' . $this->ext .
+           '?w=' . $width . '&h=' . $height . '&mode=fit&fm=' . $fm;
+  }
+
   public function productsUsedBy() {
     return $this->has_many_through('Product');
   }
