2144f88c0f5a63669c98bfa6ba1e08d00303db90
jim winstead <jimw@trainedmonkey.com>
2023-08-10 13:21:13 -0700

    Pull vendor login info from config, not constants

--- lib/Scat/Service/VendorData.php
+++ lib/Scat/Service/VendorData.php
@@ -4,6 +4,7 @@ namespace Scat\Service;
 class VendorData
 {
   public function __construct(
+    private Config $config,
     private Data $data
   ) {
   }
@@ -885,203 +886,210 @@ class VendorData
     // XXX hardcoded stuff
     switch ($vendor_item->vendor_id) {
     case 7: // Mac
-      return check_mac_stock($vendor_item->vendor_sku);
+      return $this->check_mac_stock($vendor_item->vendor_sku);
     case 3757: // SLS
-      return check_sls_stock($vendor_item->vendor_sku);
+      return $this->check_sls_stock($vendor_item->vendor_sku);
     case 30803: // PA Dist
-      return check_padist_stock($vendor_item->vendor_sku);
+      return $this->check_padist_stock($vendor_item->vendor_sku);
     case 44466: // Notions
-      return check_notions_stock($vendor_item->vendor_sku);
+      return $this->check_notions_stock($vendor_item->vendor_sku);
     default:
       throw new \Exception("Don't know how to check stock for that vendor.");
       return [];
     }
   }
-}
 
-function check_mac_stock($code) {
-  $url= 'https://www.macphersonart.com/cgi-bin/maclive/wam_tmpl/mac_cart.p';
+  function check_mac_stock($code) {
+    $url= 'https://www.macphersonart.com/cgi-bin/maclive/wam_tmpl/mac_cart.p';
 
-  $client= new \GuzzleHttp\Client();
-  $jar= \GuzzleHttp\Cookie\CookieJar::fromArray(['liveWAMSession' => MAC_KEY],
-                                                parse_url($url, PHP_URL_HOST));
+    $key= $this->config->get('vendor.macp.key');
 
-  $res= $client->request('GET', $url,
-                         [
-                         //'debug' => true,
-                           'cookies' => $jar,
-                           'query' => [
-                             'site' => 'MAC',
-                             'layout' => 'Responsive',
-                             'nocache' => 66439,
-                             'content' => 'JSON',
-                             'page' => 'mac_cart',
-                             'action' => 'getItemInfo',
-                             'itemNumber' => $code,
-                             'dropship' => 'false'
-                           ]
-                         ]);
+    $client= new \GuzzleHttp\Client();
+    $jar= \GuzzleHttp\Cookie\CookieJar::fromArray([ 'liveWAMSession' => $key ],
+                                                  parse_url($url, \PHP_URL_HOST));
 
-  $body= $res->getBody();
-  if ($GLOBALS['DEBUG']) {
-    error_log($body);
-  }
+    $res= $client->request('GET', $url,
+                           [
+                           //'debug' => true,
+                             'cookies' => $jar,
+                             'query' => [
+                               'site' => 'MAC',
+                               'layout' => 'Responsive',
+                               'nocache' => 66439,
+                               'content' => 'JSON',
+                               'page' => 'mac_cart',
+                               'action' => 'getItemInfo',
+                               'itemNumber' => $code,
+                               'dropship' => 'false'
+                             ]
+                           ]);
 
-  $data= json_decode($body);
-  $avail= [];
-
-  foreach ($data->response->itemInfo as $item) {
-    foreach ($item->itemQty as $qty) {
-      $avail[$qty->warehouseName]= $qty->qty;
+    $body= $res->getBody();
+    if ($GLOBALS['DEBUG']) {
+      error_log($body);
     }
+
+    $data= json_decode($body);
+    $avail= [];
+
+    foreach ($data->response->itemInfo as $item) {
+      foreach ($item->itemQty as $qty) {
+        $avail[$qty->warehouseName]= $qty->qty;
+      }
+    }
+
+    return $avail;
   }
 
-  return $avail;
-}
+  function check_sls_stock($code) {
+    $client= new \GuzzleHttp\Client();
+    $jar= new \GuzzleHttp\Cookie\CookieJar();
 
-function check_sls_stock($code) {
-  $client= new \GuzzleHttp\Client();
-  $jar= new \GuzzleHttp\Cookie\CookieJar();
+    $url= 'https://www.slsarts.com/loginpage.asp';
 
-  $url= 'https://www.slsarts.com/loginpage.asp';
+    $user= $this->config->get('vendor.sls.username');
+    $pass= $this->config->get('vendor.sls.password');
 
-  $res= $client->request('POST', $url,
-                         [
-                         //'debug' => true,
-                           'cookies' => $jar,
-                           'form_params' => [
-                             'level1' => '',
-                             'level2' => '',
-                             'level3' => '',
-                             'level4' => '',
-                             'level5' => '',
-                             'skuonly' => '',
-                             'txtfind' => '',
-                             'snum' => '',
-                             'skey' => '',
-                             'username' => SLS_USER,
-                             'password' => SLS_PASSWORD,
-                             'btnlogin' => 'Login'
-                           ]
-                         ]);
+    $res= $client->request('POST', $url,
+                           [
+                           //'debug' => true,
+                             'cookies' => $jar,
+                             'form_params' => [
+                               'level1' => '',
+                               'level2' => '',
+                               'level3' => '',
+                               'level4' => '',
+                               'level5' => '',
+                               'skuonly' => '',
+                               'txtfind' => '',
+                               'snum' => '',
+                               'skey' => '',
+                               'username' => $user,
+                               'password' => $pass,
+                               'btnlogin' => 'Login'
+                             ]
+                           ]);
 
-  $url= 'https://www.slsarts.com/viewcarttop.asp';
-  $res= $client->request('POST', $url,
-                         [
-                         //'debug' => true,
-                           'cookies' => $jar,
-                           'form_params' => [
-                             'defwh' => 2,
-                             'imaacr' => '',
-                             'forcebottom' => 'N',
-                             'slssku' => $code,
-                             'slsskuorig' => '',
-                             'slsqty' => '',
-                             'WH' => 2,
-                             'oldwh' => '',
-                             'goclear' => '',
-                             'vmwvnm' => '',
-                             'imadsc' => '',
-                             'msrp' => '',
-                             'minqty' => '',
-                             'qoh1' => '',
-                             'price' => '',
-                             'disc' => 0,
-                             'qoh2' => '',
-                             'borderitem' => 'Order Item',
-                             'verror' => '',
-                             'vonblur' => 'Y',
-                             'vedititem' => '',
-                             'veditwh' => '',
-                             'vdelitem' => '',
-                             'vdropship' => 'E',
-                             'deletelist' => '',
-                             'thepos' => '',
-                           ]
-                         ]);
+    $url= 'https://www.slsarts.com/viewcarttop.asp';
+    $res= $client->request('POST', $url,
+                           [
+                           //'debug' => true,
+                             'cookies' => $jar,
+                             'form_params' => [
+                               'defwh' => 2,
+                               'imaacr' => '',
+                               'forcebottom' => 'N',
+                               'slssku' => $code,
+                               'slsskuorig' => '',
+                               'slsqty' => '',
+                               'WH' => 2,
+                               'oldwh' => '',
+                               'goclear' => '',
+                               'vmwvnm' => '',
+                               'imadsc' => '',
+                               'msrp' => '',
+                               'minqty' => '',
+                               'qoh1' => '',
+                               'price' => '',
+                               'disc' => 0,
+                               'qoh2' => '',
+                               'borderitem' => 'Order Item',
+                               'verror' => '',
+                               'vonblur' => 'Y',
+                               'vedititem' => '',
+                               'veditwh' => '',
+                               'vdelitem' => '',
+                               'vdropship' => 'E',
+                               'deletelist' => '',
+                               'thepos' => '',
+                             ]
+                           ]);
 
-  $body= $res->getBody();
+    $body= $res->getBody();
 
-  $dom= new \DOMDocument();
-  libxml_use_internal_errors(true);
-  $dom->loadHTML($body);
+    $dom= new \DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($body);
 
-  $xp= new \DOMXpath($dom);
-  $no= $xp->query('//input[@name="qoh1"]')->item(0)->getAttribute('value');
-  $vg= $xp->query('//input[@name="qoh2"]')->item(0)->getAttribute('value');
+    $xp= new \DOMXpath($dom);
+    $no= $xp->query('//input[@name="qoh1"]')->item(0)->getAttribute('value');
+    $vg= $xp->query('//input[@name="qoh2"]')->item(0)->getAttribute('value');
 
-  return [ 'Vegas' => $vg, 'New Orleans' => $no ];
-}
-
-function check_padist_stock($code) {
-  $lookup_url= 'https://www.pa-dist.com/ajax/search/suggestions';
-
-  $client= new \GuzzleHttp\Client();
-  $jar= \GuzzleHttp\Cookie\CookieJar::fromArray(
-    [ 'WebLogin' => PADIST_KEY ],
-    parse_url($lookup_url, PHP_URL_HOST)
-  );
-
-  $res= $client->request('GET', $lookup_url,
-                         [
-                           'cookies' => $jar,
-                           'query' => [
-                             'q' => $code
-                           ]
-                         ]);
-
-  $body= $res->getBody();
-  if ($GLOBALS['DEBUG']) {
-    error_log($body);
+    return [ 'Vegas' => $vg, 'New Orleans' => $no ];
   }
 
-  // gross, but we're parsing html with a regex. ¯\_(ツ)_/¯
-  if (preg_match("!/Item/(\d+)[^']+'><item-number><i>" . preg_quote($code) . '</i></item-number>!', $body, $m)) {
-    $id= $m[1];
-  } else {
-    throw new \Exception("Unable to find ID for $code");
+  function check_padist_stock($code) {
+    $lookup_url= 'https://www.pa-dist.com/ajax/search/suggestions';
+
+    $key= $this->config->get('vendor.padist.key');
+
+    $client= new \GuzzleHttp\Client();
+    $jar= \GuzzleHttp\Cookie\CookieJar::fromArray(
+      [ 'WebLogin' => $key ],
+      parse_url($lookup_url, PHP_URL_HOST)
+    );
+
+    $res= $client->request('GET', $lookup_url,
+                           [
+                             'cookies' => $jar,
+                             'query' => [
+                               'q' => $code
+                             ]
+                           ]);
+
+    $body= $res->getBody();
+    if ($GLOBALS['DEBUG']) {
+      error_log($body);
+    }
+
+    // gross, but we're parsing html with a regex. ¯\_(ツ)_/¯
+    if (preg_match("!/Item/(\d+)[^']+'><item-number><i>" . preg_quote($code) . '</i></item-number>!', $body, $m)) {
+      $id= $m[1];
+    } else {
+      throw new \Exception("Unable to find ID for $code");
+    }
+
+    $details_url= 'https://www.pa-dist.com/ajax/item/detail/' . $id;
+
+    $res= $client->request('GET', $details_url,
+                           [
+                             'cookies' => $jar,
+                           ]);
+
+    $body= $res->getBody();
+    if ($GLOBALS['DEBUG']) {
+      error_log($body);
+    }
+
+    $dom= new \DOMDocument();
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($body);
+
+    $xp= new \DOMXpath($dom);
+    $stock= $xp->query('//span[@class="StockLevel"]')->item(0)->textContent;
+
+    return [ 'stock' => $stock ];
   }
 
-  $details_url= 'https://www.pa-dist.com/ajax/item/detail/' . $id;
+  function check_notions_stock($code) {
+    $lookup_url= 'https://my.notionsmarketing.com/integration-web-services/services_rest/erpservice/qasInquiry';
 
-  $res= $client->request('GET', $details_url,
-                         [
-                           'cookies' => $jar,
-                         ]);
+    $client= new \GuzzleHttp\Client();
 
-  $body= $res->getBody();
-  if ($GLOBALS['DEBUG']) {
-    error_log($body);
+    $res= $client->request('POST', $lookup_url, [
+      'headers' => [
+        'Content-type' => 'application/json',
+        'Accept' => 'application/json'
+      ],
+      'json' => [
+        'skulist' => sprintf('%06d', $code)
+      ]
+    ]);
+
+    $body= $res->getBody();
+
+    $data= json_decode($body);
+
+    return [ 'stock' => $data->qas ];
   }
-
-  $dom= new \DOMDocument();
-  libxml_use_internal_errors(true);
-  $dom->loadHTML($body);
-
-  $xp= new \DOMXpath($dom);
-  $stock= $xp->query('//span[@class="StockLevel"]')->item(0)->textContent;
-
-  return [ 'stock' => $stock ];
-}
-
-function check_notions_stock($code) {
-  $lookup_url= 'https://my.notionsmarketing.com/integration-web-services/services_rest/erpservice/qasInquiry';
-
-  $client= new \GuzzleHttp\Client();
-
-  $res= $client->request('POST', $lookup_url, [
-    'headers' => [
-      'Content-type' => 'application/json',
-      'Accept' => 'application/json'
-    ],
-    'json' => [
-      'skulist' => sprintf('%06d', $code)
-    ]
-  ]);
-
-  $body= $res->getBody();
-
-  $data= json_decode($body);
-
-  return [ 'stock' => $data->qas ];
 }
