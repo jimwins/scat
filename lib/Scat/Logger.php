a66e6412d82c04376ed1dbb5851c4e414cf91440
jim winstead <jimw@trainedmonkey.com>
2023-09-28 10:10:41 -0700

    Drop support for Sentry, brings in out-of-date dependencies

--- lib/Scat/Logger.php
+++ lib/Scat/Logger.php
@@ -8,12 +8,5 @@ class Logger extends \Monolog\Logger
 
     $streamHandler= new \Monolog\Handler\StreamHandler('php://stderr');
     $this->pushHandler($streamHandler);
-
-    $sentry_dsn= $config->get('sentry.dsn');
-    if ($sentry_dsn) {
-      \Sentry\init([ 'dsn' => $sentry_dsn ]);
-      $sentry= new \Sentry\Monolog\Handler(\Sentry\SentrySdk::getCurrentHub());
-      $this->pushHandler($sentry);
-    }
   }
 }
