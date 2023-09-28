61907ae4a518d82ac636df18aaf6aebaf6735921
jim winstead <jimw@trainedmonkey.com>
2023-09-28 10:24:21 -0700

    Fix case of class name

--- lib/Scat/Middleware/NoIndex.php
+++ lib/Scat/Middleware/NoIndex.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-final class Noindex implements MiddlewareInterface
+final class NoIndex implements MiddlewareInterface
 {
   public function process(
     ServerRequestInterface $request,
