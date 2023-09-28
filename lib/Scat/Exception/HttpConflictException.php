a0fca3ff7ca5a2377cb9c1a2b849ce90ba250a9c
jim winstead <jimw@trainedmonkey.com>
2023-09-28 10:25:35 -0700

    Add types for properties

--- lib/Scat/Exception/HttpConflictException.php
+++ lib/Scat/Exception/HttpConflictException.php
@@ -5,6 +5,6 @@ class HttpConflictException extends \Slim\Exception\HttpException
 {
     protected $code= 409;
     protected $message= 'Conflict.';
-    protected $title= '409 Conflict';
-    protected $description= 'The request could not be completed due to a conflict with the current state of the target resource.';
+    protected string $title= '409 Conflict';
+    protected string $description= 'The request could not be completed due to a conflict with the current state of the target resource.';
 }
